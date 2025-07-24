ff63c23e19b546445e33baa566f5d6a9690e5037
confused-moniker <ralph@genfhi.app>
2025-07-23 21:12:52 -0500

    mod

--- crates/reflect/src/traits.rs
+++ crates/reflect/src/traits.rs
@@ -4,7 +4,11 @@ pub trait MetaValue: Any + Debug {
     fn fields(&self) -> Vec<&'static str>;
 
     fn get_field<'a>(&'a self, field: &str) -> Option<&'a dyn MetaValue>;
+    fn get_field_mut<'a>(&'a mut self, field: &str) -> Option<&'a mut dyn MetaValue>;
+
     fn get_index<'a>(&'a self, index: usize) -> Option<&'a dyn MetaValue>;
+    fn get_index_mut<'a>(&'a mut self, index: usize) -> Option<&'a mut dyn MetaValue>;
+
     fn flatten(&self) -> Vec<&dyn MetaValue>;
 
     fn as_any(&self) -> &dyn Any;
