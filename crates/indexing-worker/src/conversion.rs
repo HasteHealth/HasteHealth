d63f22f5d6a08b1fdc459e22c2c75de312d9973f
confused-moniker <ralph@genfhi.app>
2025-07-29 13:57:15 -0500

    mod

--- crates/indexing-worker/src/conversion.rs
+++ crates/indexing-worker/src/conversion.rs
@@ -1,5 +1,6 @@
+/// Reference of conversions found here https://www.hl7.org/fhir/R4/search.html#table
 use oxidized_fhir_model::r4::types::{
-    FHIRDecimal, FHIRInteger, FHIRPositiveInt, FHIRUnsignedInt, SearchParameter,
+    FHIRDecimal, FHIRInteger, FHIRPositiveInt, FHIRString, FHIRUnsignedInt, SearchParameter,
 };
 use oxidized_fhir_operation_error::{OperationOutcomeError, derive::OperationOutcomeError};
 use oxidized_reflect::MetaValue;
@@ -22,39 +23,36 @@ enum InsertableIndexError {
     FailedDowncast(String),
 }
 
+// "http://hl7.org/fhirpath/System.String" => value
+//     .as_any()
+//     .downcast_ref::<String>()
+//     .map(|v| vec![v.clone()])
+//     .ok_or_else(|| InsertableIndexError::FailedDowncast(value.typename().to_string())),
+
+fn convert_fp_string(value: &FHIRString) -> Vec<String> {
+    value
+        .value
+        .as_ref()
+        .map(|v| vec![v.to_string()])
+        .unwrap_or_else(|| vec![])
+}
+
+fn convert_optional_fp_string(value: &Option<Box<FHIRString>>) -> Vec<String> {
+    value
+        .as_ref()
+        .map(|v| convert_fp_string(v))
+        .unwrap_or_else(|| vec![])
+}
+
+fn convert_optional_fp_string_vec(value: &Option<Vec<Box<FHIRString>>>) -> Vec<String> {
+    value
+        .as_ref()
+        .map(|v| v.iter().flat_map(|s| convert_fp_string(s)).collect())
+        .unwrap_or_else(|| vec![])
+}
+
 fn index_string(value: &dyn MetaValue) -> Result<Vec<String>, InsertableIndexError> {
     match value.typename() {
-        "FHIRBase64Binary" => {
-            let fp_base64 = value
-                .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRBase64Binary>()
-                .ok_or_else(|| {
-                    InsertableIndexError::FailedDowncast(value.typename().to_string())
-                })?;
-            Ok(fp_base64.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
-        }
-        "FHIRCanonical" => {
-            let fp_canonical = value
-                .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRCanonical>()
-                .ok_or_else(|| {
-                    InsertableIndexError::FailedDowncast(value.typename().to_string())
-                })?;
-            Ok(fp_canonical
-                .value
-                .map(|v| vec![v])
-                .unwrap_or_else(|| vec![]))
-        }
-
-        "FHIRCode" => {
-            let fp_code = value
-                .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRCode>()
-                .ok_or_else(|| {
-                    InsertableIndexError::FailedDowncast(value.typename().to_string())
-                })?;
-            Ok(fp_code.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
-        }
         "FHIRString" => {
             let fp_string = value
                 .as_any()
@@ -62,58 +60,60 @@ fn index_string(value: &dyn MetaValue) -> Result<Vec<String>, InsertableIndexErr
                 .ok_or_else(|| {
                     InsertableIndexError::FailedDowncast(value.typename().to_string())
                 })?;
-            Ok(fp_string.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
+            Ok(fp_string
+                .value
+                .as_ref()
+                .map(|v| vec![v.to_string()])
+                .unwrap_or_else(|| vec![]))
         }
-        "FHIROid" => {
-            let fp_oid = value
+        // Even though spec states won't encounter this it does. [ImplementationGuide.description]
+        "FHIRMarkdown" => {
+            let fp_markdown = value
                 .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIROid>()
+                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRMarkdown>()
                 .ok_or_else(|| {
                     InsertableIndexError::FailedDowncast(value.typename().to_string())
                 })?;
-            Ok(fp_oid.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
+            Ok(fp_markdown
+                .value
+                .as_ref()
+                .map(|v| vec![v.to_string()])
+                .unwrap_or_else(|| vec![]))
         }
-        "FHIRUri" => {
-            let fp_uri = value
+        "HumanName" => {
+            let human_name = value
                 .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRUri>()
+                .downcast_ref::<oxidized_fhir_model::r4::types::HumanName>()
                 .ok_or_else(|| {
                     InsertableIndexError::FailedDowncast(value.typename().to_string())
                 })?;
-            Ok(fp_uri.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
+
+            let mut string_index = vec![];
+            string_index.extend(convert_optional_fp_string(&human_name.text));
+            string_index.extend(convert_optional_fp_string(&human_name.family));
+            string_index.extend(convert_optional_fp_string_vec(&human_name.given));
+            string_index.extend(convert_optional_fp_string_vec(&human_name.prefix));
+            string_index.extend(convert_optional_fp_string_vec(&human_name.suffix));
+            Ok(string_index)
         }
-        "FHIRUrl" => {
-            let fp_url = value
+        "Address" => {
+            let mut string_index = vec![];
+            let address = value
                 .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRUrl>()
+                .downcast_ref::<oxidized_fhir_model::r4::types::Address>()
                 .ok_or_else(|| {
                     InsertableIndexError::FailedDowncast(value.typename().to_string())
                 })?;
-            Ok(fp_url.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
+            string_index.extend(convert_optional_fp_string(&address.text));
+            string_index.extend(convert_optional_fp_string_vec(&address.line));
+            string_index.extend(convert_optional_fp_string(&address.city));
+            string_index.extend(convert_optional_fp_string(&address.district));
+            string_index.extend(convert_optional_fp_string(&address.state));
+            string_index.extend(convert_optional_fp_string(&address.postalCode));
+            string_index.extend(convert_optional_fp_string(&address.country));
+
+            Ok(string_index)
         }
-        "FHIRUuid" => {
-            let fp_uuid = value
-                .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRUuid>()
-                .ok_or_else(|| {
-                    InsertableIndexError::FailedDowncast(value.typename().to_string())
-                })?;
-            Ok(fp_uuid.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
-        }
-        "FHIRXhtml" => {
-            let fp_xhtml = value
-                .as_any()
-                .downcast_ref::<oxidized_fhir_model::r4::types::FHIRXhtml>()
-                .ok_or_else(|| {
-                    InsertableIndexError::FailedDowncast(value.typename().to_string())
-                })?;
-            Ok(fp_xhtml.value.map(|v| vec![v]).unwrap_or_else(|| vec![]))
-        }
-        "http://hl7.org/fhirpath/System.String" => value
-            .as_any()
-            .downcast_ref::<String>()
-            .map(|v| vec![v.clone()])
-            .ok_or_else(|| InsertableIndexError::FailedDowncast(value.typename().to_string())),
 
         type_name => Err(InsertableIndexError::FailedDowncast(type_name.to_string())),
     }
