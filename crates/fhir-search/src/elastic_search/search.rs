3ed1dc9b1d48ee6380859b00cd1853340955d08f
confused-moniker <ralph@genfhi.app>
2025-08-07 11:43:37 -0500

    mod

--- crates/fhir-search/src/elastic_search/search.rs
+++ crates/fhir-search/src/elastic_search/search.rs
@@ -1,10 +1,61 @@
 use oxidized_fhir_client::url::ParsedParameter;
+use oxidized_fhir_model::r4::types::ResourceType;
+use oxidized_fhir_operation_error::derive::OperationOutcomeError;
 use oxidized_fhir_repository::VersionIdRef;
 
-pub enum QueryBuildError {}
+use crate::SearchRequest;
 
-fn build_elastic_search(
-    parameters: Vec<ParsedParameter>,
+#[derive(OperationOutcomeError, Debug)]
+pub enum QueryBuildError {
+    #[error(
+        code = "not-found",
+        diagnostic = "Search parameter with name '{arg1}' not found for resource type '{arg0:?}'"
+    )]
+    MissingParameter(ResourceType, String),
+    #[error(
+        code = "invalid",
+        diagnostic = "Unsupported search request type or parameter: {arg0}"
+    )]
+    UnsupportedParameter(String),
+}
+
+fn build_elastic_search_query(
+    request: &SearchRequest,
 ) -> Result<Vec<VersionIdRef<'static>>, QueryBuildError> {
-    todo!();
+    match request {
+        SearchRequest::TypeSearch(type_search_request) => {
+            let resource_type = &type_search_request.resource_type;
+            let parameters = &type_search_request.parameters;
+
+            for parameter in parameters.iter() {
+                match parameter {
+                    ParsedParameter::Resource(resource_param) => {
+                        let search_param =
+                            oxidized_artifacts::search_parameters::get_search_parameter_for_name(
+                                &resource_type,
+                                &resource_param.name,
+                            )
+                            .ok_or_else(|| {
+                                QueryBuildError::MissingParameter(
+                                    resource_type.clone(),
+                                    resource_param.name.to_string(),
+                                )
+                            })?;
+                    }
+                    ParsedParameter::Result(result_param) => {
+                        return Err(QueryBuildError::UnsupportedParameter(
+                            result_param.name.to_string(),
+                        ));
+                    }
+                }
+            }
+
+            todo!();
+        }
+        _ => {
+            return Err(QueryBuildError::UnsupportedParameter(
+                "Unsupported search request type".to_string(),
+            ));
+        }
+    }
 }
