36e5c83ae6b351768fc3e7330ad7ec19e177a4f9
confused-moniker <ralph@genfhi.app>
2025-08-09 16:48:59 -0500

    mod

--- crates/server/src/auth_n/oidc/routes/mod.rs
+++ crates/server/src/auth_n/oidc/routes/mod.rs
@@ -5,19 +5,22 @@ use axum_extra::routing::{
     RouterExt, // for `Router::typed_*`
     TypedPath,
 };
+use oxidized_fhir_repository::TenantId;
 use serde::{Deserialize, Serialize};
 
 // A type safe route with `/users/{id}` as its associated path.
 #[derive(TypedPath, Deserialize)]
-#[typed_path("/token/{id}")]
+#[typed_path("/{tenant}/token/{id}")]
 pub struct TokenPostRoute {
+    tenant: TenantId,
     id: String,
 }
 
 // A type safe route with `/users/{id}` as its associated path.
 #[derive(TypedPath, Deserialize)]
-#[typed_path("/token/{id}")]
+#[typed_path("/{tenant}/token/{id}")]
 pub struct TokenGetRoute {
+    tenant: TenantId,
     pub id: String,
 }
 
@@ -41,11 +44,11 @@ async fn well_known(_: WellKnown) -> Result<Json<OIDCResponse>, String> {
     Ok(Json(oidc_response))
 }
 
-async fn token_get(TokenGetRoute { id }: TokenGetRoute) -> String {
+async fn token_get(TokenGetRoute { tenant, id }: TokenGetRoute) -> String {
     id
 }
 
-async fn token_post(TokenPostRoute { id }: TokenPostRoute) -> String {
+async fn token_post(TokenPostRoute { tenant, id }: TokenPostRoute) -> String {
     id
 }
 
