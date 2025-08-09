4bb0dba1399a8082068c6aa44183450f282632ee
confused-moniker <ralph@genfhi.app>
2025-08-09 15:08:45 -0500

    test cases

--- crates/server/src/auth_n/oidc/schemas/mod.rs
+++ crates/server/src/auth_n/oidc/schemas/mod.rs
@@ -7,3 +7,54 @@ pub mod token_instrospection {
         schema = "./src/auth_n/oidc/schemas/oauth2_token_introspection.schema.json"
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_token_body() {
+        let body = serde_json::from_str::<token_body::OAuth2TokenBody>(
+            r#"
+            {
+               "grant_type": "refresh_token",
+               "refresh_token": "hello",
+               "scope" : "read write",
+               "client_id": "test_client",
+               "client_secret": "test_secret"
+            }
+            "#,
+        );
+
+        assert!(body.is_ok());
+
+        let body = serde_json::from_str::<token_body::OAuth2TokenBody>(
+            r#"
+            {
+               "grant_type": "refresh_token",
+               "refresh_token": "hello",
+               "scope" : "read write",
+               "client_id": "test_client",
+               "client_secret": "test_secret",
+               "code": "should not be here"
+            }
+            "#,
+        );
+
+        assert!(!body.is_ok());
+
+        let body = serde_json::from_str::<token_body::OAuth2TokenBody>(
+            r#"
+            {
+                "grant_type": "authorization_code",
+                "code": "code",
+                "redirect_uri": "redirect_uri",
+                "code_verifier": "code_verifier",
+                "client_id": "client_id"
+            }
+            "#,
+        );
+
+        assert!(body.is_ok());
+    }
+}
