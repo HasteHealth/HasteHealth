95c62a84d066924f09e45de10f78f81ee93bee66
confused-moniker <ralph@genfhi.app>
2025-08-09 17:09:03 -0500

    mod

--- crates/server/src/auth_n/oidc/routes/mod.rs
+++ crates/server/src/auth_n/oidc/routes/mod.rs
@@ -1,13 +1,23 @@
 use std::sync::Arc;
 
-use axum::{Router, extract::Json};
+use axum::{
+    Router,
+    body::Body,
+    extract::{Json, Request, State},
+    response::Response,
+};
 use axum_extra::routing::{
     RouterExt, // for `Router::typed_*`
     TypedPath,
 };
-use oxidized_fhir_repository::TenantId;
+use oxidized_fhir_client::FHIRClient;
+use oxidized_fhir_model::r4::types::ResourceType;
+use oxidized_fhir_repository::{Author, FHIRRepository, ProjectId, TenantId};
+use oxidized_fhir_search::SearchEngine;
 use serde::{Deserialize, Serialize};
 use tower::ServiceBuilder;
+
+use crate::{AppState, server_client::ServerCTX};
 
 // A type safe route with `/users/{id}` as its associated path.
 #[derive(TypedPath, Deserialize)]
@@ -53,18 +63,56 @@ async fn token_post(TokenPostRoute { tenant, id }: TokenPostRoute) -> String {
     id
 }
 
-pub fn create_router<T: Send + Sync + 'static>() -> Router<Arc<T>> {
+async fn middleware_test<
+    Repo: FHIRRepository + Send + Sync + 'static,
+    Search: SearchEngine + Send + Sync + 'static,
+>(
+    State(state): State<Arc<AppState<Repo, Search>>>,
+    req: Request<Body>,
+    next: axum::middleware::Next,
+) -> Response<Body> {
+    let ctx = ServerCTX {
+        tenant: TenantId::new("tenant".to_string()),
+        project: ProjectId::new("project".to_string()),
+        fhir_version: oxidized_fhir_repository::SupportedFHIRVersions::R4,
+        author: Author {
+            id: "anonymous".to_string(),
+            kind: "Membership".to_string(),
+        },
+    };
+
+    let res = state
+        .fhir_client
+        .search_type(
+            ctx,
+            ResourceType::new("ClientApplication".to_string()).unwrap(),
+            vec![],
+        )
+        .await;
+    println!("{:?}", res);
+
+    let response = next.run(req).await;
+
+    // do something with `response`...
+
+    response
+}
+
+pub fn create_router<
+    T: Send + Sync + 'static,
+    Repo: FHIRRepository + Send + Sync + 'static,
+    Search: SearchEngine + Send + Sync + 'static,
+>(
+    state: Arc<AppState<Repo, Search>>,
+) -> Router<Arc<T>> {
     Router::new()
         .typed_get(token_get)
         .typed_post(token_post)
         .typed_get(well_known)
-        .layer(ServiceBuilder::new().layer(axum::middleware::from_fn(
-            |req, next: axum::middleware::Next| {
-                async move {
-                    // println!("Request: {:?}", req);
-                    // Middleware logic can be added here
-                    next.run(req).await
-                }
-            },
-        )))
+        .layer(
+            ServiceBuilder::new().layer(axum::middleware::from_fn_with_state(
+                state.clone(),
+                middleware_test,
+            )),
+        )
 }
