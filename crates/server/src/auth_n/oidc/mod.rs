e5ef7afbfec5ad4be17a5c76022b8b2b73ed9968
confused-moniker <ralph@genfhi.app>
2025-08-09 20:32:59 -0500

    rm

--- crates/server/src/auth_n/oidc/mod.rs
+++ crates/server/src/auth_n/oidc/mod.rs
@@ -3,15 +3,3 @@ pub mod schemas;
 
 pub mod hardcoded_clients;
 mod middleware;
-
-fn tester() {
-    let k = schemas::token_instrospection::OAuth2TokenIntrospectionBody {
-        token: "TEST".to_string(),
-    };
-    schemas::token_body::OAuth2TokenBody::RefreshToken {
-        client_id: Some("TEST".to_string()),
-        client_secret: Some("TEST".to_string()),
-        refresh_token: "TEST".to_string(),
-        scope: None,
-    };
-}
