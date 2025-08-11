baadcbe188b2c50602d994fb430f38e4a64432a0
confused-moniker <ralph@genfhi.app>
2025-08-11 12:46:40 -0500

    rm

--- crates/server/src/auth_n/oidc/hardcoded_clients/mod.rs
+++ crates/server/src/auth_n/oidc/hardcoded_clients/mod.rs
@@ -1,14 +1,10 @@
 use oxidized_config::Config;
 use oxidized_fhir_model::r4::types::ClientApplication;
-use oxidized_fhir_repository::TenantId;
 
 pub mod admin_app;
 
 #[allow(dead_code)]
-pub fn get_hardcoded_clients(
-    config: Box<dyn Config>,
-    tenant_id: TenantId,
-) -> Vec<ClientApplication> {
+pub fn get_hardcoded_clients(config: Box<dyn Config>) -> Vec<ClientApplication> {
     let mut hardcoded_apps = vec![];
 
     if let Some(app) = admin_app::get_admin_app(config) {
