10485c2d0ce3b8ba05a84685a72967347bf0854f
confused-moniker <ralph@genfhi.app>
2025-08-11 18:44:20 -0500

    mod

---
+++ crates/server/migrations/20250811233547_authorization_codes.sql
@@ -0,0 +1,38 @@
+CREATE TYPE code_kind AS ENUM (
+    'password_reset',
+    'oauth2_code_grant',
+    'refresh_token'
+);
+
+CREATE TYPE pkce_method AS ENUM ('S256', 'plain');
+
+CREATE TABLE
+    authorization_code (
+        id uuid DEFAULT gen_random_uuid () NOT NULL PRIMARY KEY,
+        kind code_kind NOT NULL,
+        code text NOT NULL,
+        client_id text,
+        tenant text,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        expires_in interval NOT NULL,
+        user_id text NOT NULL,
+        pkce_code_challenge text,
+        pkce_code_challenge_method pkce_method,
+        redirect_uri character varying(255),
+        meta jsonb,
+        CONSTRAINT authorization_code_code_key UNIQUE (code),
+        CONSTRAINT fk_tenant FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE,
+        CONSTRAINT fk_user FOREIGN KEY (tenant, user_id) REFERENCES users (tenant, fhir_user_id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    authorization_scopes (
+        client_id text NOT NULL,
+        user_id text NOT NULL,
+        scope text NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
+        tenant text NOT NULL,
+        CONSTRAINT authorization_scopes_pkey PRIMARY KEY (tenant, client_id, user_id),
+        CONSTRAINT authorization_scopes_tenant_fkey FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE,
+        CONSTRAINT fk_user FOREIGN KEY (tenant, user_id) REFERENCES users (tenant, fhir_user_id) ON DELETE CASCADE
+    );
