c3a90b039dfa819b59fdc21369ddd4cb4d6dade3
confused-moniker <ralph@genfhi.app>
2025-08-13 13:22:47 -0500

    mod

---
+++ crates/server/src/lib.rs
@@ -0,0 +1,152 @@
+use crate::{
+    fhir_http::{HTTPRequest, http_request_to_fhir_request},
+    pg::get_pool,
+    server_client::{FHIRServerClient, ServerCTX},
+};
+use axum::{
+    Router,
+    extract::{OriginalUri, Path, State},
+    http::Method,
+    response::{IntoResponse, Response},
+    routing::any,
+};
+use oxidized_config::{Config, get_config};
+use oxidized_fhir_client::FHIRClient;
+use oxidized_fhir_operation_error::{OperationOutcomeError, derive::OperationOutcomeError};
+use oxidized_fhir_repository::{
+    Author, FHIRRepository, ProjectId, SupportedFHIRVersions, TenantId, postgres::PGConnection,
+};
+use oxidized_fhir_search::SearchEngine;
+use oxidized_fhirpath::FPEngine;
+use serde::Deserialize;
+use std::{env::VarError, sync::Arc, time::Instant};
+use tower_http::services::ServeDir;
+use tower_sessions::SessionManagerLayer;
+use tower_sessions_sqlx_store::PostgresStore;
+use tracing::info;
+
+mod auth_n;
+mod extract;
+mod fhir_http;
+mod pg;
+mod server_client;
+
+#[derive(OperationOutcomeError, Debug)]
+pub enum ConfigError {
+    #[error(code = "invalid", diagnostic = "Invalid environment!")]
+    DotEnv(#[from] dotenvy::Error),
+    #[error(code = "invalid", diagnostic = "Invalid session!")]
+    Session(#[from] tower_sessions::session::Error),
+    #[error(code = "invalid", diagnostic = "Database error")]
+    Database(#[from] sqlx::Error),
+    #[error(code = "invalid", diagnostic = "Environment variable not set {arg0}")]
+    EnvironmentVariable(#[from] VarError),
+    #[error(code = "invalid", diagnostic = "Failed to render template.")]
+    TemplateRender,
+}
+
+#[derive(OperationOutcomeError, Debug)]
+pub enum CustomOpError {
+    #[error(code = "invalid", diagnostic = "FHIRPath error")]
+    FHIRPath(#[from] oxidized_fhirpath::FHIRPathError),
+    #[error(code = "invalid", diagnostic = "Failed to deserialize resource")]
+    Deserialize(#[from] serde_json::Error),
+    #[error(code = "invalid", diagnostic = "Internal server error")]
+    InternalServerError,
+}
+
+pub struct AppState<
+    Repo: FHIRRepository + Send + Sync + 'static,
+    Search: SearchEngine + Send + Sync + 'static,
+> {
+    fhir_client: FHIRServerClient<Repo, Search>,
+    _config: Box<dyn Config>,
+}
+
+#[derive(Deserialize)]
+struct FHIRHandlerPath {
+    tenant: TenantId,
+    project: ProjectId,
+    fhir_version: SupportedFHIRVersions,
+    fhir_location: String,
+}
+
+async fn fhir_handler<
+    Repo: FHIRRepository + Send + Sync + 'static,
+    Search: SearchEngine + Send + Sync + 'static,
+>(
+    method: Method,
+    OriginalUri(uri): OriginalUri,
+    Path(path): Path<FHIRHandlerPath>,
+    State(state): State<Arc<AppState<Repo, Search>>>,
+    body: String,
+) -> Result<Response, OperationOutcomeError> {
+    let start = Instant::now();
+    info!("[{}] '{}'", method, path.fhir_location);
+
+    let http_req = HTTPRequest::new(
+        method,
+        path.fhir_location,
+        body,
+        uri.query().unwrap_or_default().to_string(),
+    );
+    let fhir_request = http_request_to_fhir_request(SupportedFHIRVersions::R4, &http_req)?;
+
+    let ctx = ServerCTX {
+        tenant: path.tenant,
+        project: path.project,
+        fhir_version: path.fhir_version,
+        author: Author {
+            id: "anonymous".to_string(),
+            kind: "Membership".to_string(),
+        },
+    };
+
+    let response = state.fhir_client.request(ctx, fhir_request).await?;
+    info!("Request processed in {:?}", start.elapsed());
+
+    Ok(response.into_response())
+}
+
+pub async fn server() -> Result<Router, OperationOutcomeError> {
+    let config = get_config("environment".into());
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let pool = get_pool(config.as_ref()).await;
+    let session_store = PostgresStore::new(pool.clone());
+    session_store.migrate().await.map_err(ConfigError::from)?;
+
+    let search_engine = oxidized_fhir_search::elastic_search::ElasticSearchEngine::new(
+        Arc::new(FPEngine::new()),
+        &config
+            .get("ELASTICSEARCH_URL")
+            .expect("ELASTICSEARCH_URL variable not set"),
+        config
+            .get("ELASTICSEARCH_USERNAME")
+            .expect("ELASTICSEARCH_USERNAME variable not set"),
+        config
+            .get("ELASTICSEARCH_PASSWORD")
+            .expect("ELASTICSEARCH_PASSWORD variable not set"),
+    )
+    .expect("Failed to create Elasticsearch client");
+
+    let shared_state = Arc::new(AppState {
+        _config: config,
+        fhir_client: FHIRServerClient::new(PGConnection::PgPool(pool.clone()), search_engine),
+    });
+
+    let project_router = Router::new()
+        .route("/fhir/{fhir_version}/{*fhir_location}", any(fhir_handler))
+        .nest("/oidc", auth_n::oidc::routes::create_router());
+
+    let tenant_router = Router::new().nest("/api/v1/{project}", project_router);
+
+    let app = Router::new()
+        .nest("/w/{tenant}", tenant_router)
+        .layer(SessionManagerLayer::new(session_store).with_secure(true))
+        .with_state(shared_state)
+        .fallback_service(ServeDir::new("public"));
+
+    Ok(app)
+}
