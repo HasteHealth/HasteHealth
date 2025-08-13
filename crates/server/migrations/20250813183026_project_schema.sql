c2f45196bc1e38bb19b53a05a8dc30ddb7d63e2c
confused-moniker <ralph@genfhi.app>
2025-08-13 15:22:16 -0500

    mod

--- crates/server/migrations/20250813183026_project_schema.sql
+++ crates/server/migrations/20250813183026_project_schema.sql
@@ -1,8 +1,11 @@
 CREATE TABLE
     project (
         tenant text NOT NULL,
-        id uuid DEFAULT gen_random_uuid () NOT NULL PRIMARY KEY,
+        id text NOT NULL,
         fhir_version fhir_version NOT NULL,
         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        CONSTRAINT fk_tenant FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE
+        CONSTRAINT fk_tenant FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE,
+        CONSTRAINT project_pkey PRIMARY KEY (tenant, id)
     );
+
+ALTER TABLE resources ADD CONSTRAINT fk_project FOREIGN KEY (tenant, project) REFERENCES project (tenant, id) ON DELETE CASCADE;
