2f150d6069a5c3c6754013ea9641be30f88e33b7
confused-moniker <ralph@genfhi.app>
2025-08-13 14:58:08 -0500

    mod

--- crates/server/migrations/20250813183026_project_schema.sql
+++ crates/server/migrations/20250813183026_project_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     project (
-        tenant text,
+        tenant text NOT NULL,
         id uuid DEFAULT gen_random_uuid () NOT NULL PRIMARY KEY,
         fhir_version fhir_version NOT NULL,
         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
