4eb8fa8a1f9eaf5c2698a58816bd979f17867afa
confused-moniker <ralph@genfhi.app>
2025-08-16 20:17:06 -0500

    mod

--- crates/server/src/auth_n/certificates/signing_keys.rs
+++ crates/server/src/auth_n/certificates/signing_keys.rs
@@ -1,19 +1,63 @@
+use oxidized_config::{ConfigType, get_config};
 use oxidized_fhir_operation_error::OperationOutcomeError;
 use rand::rngs::OsRng;
-use rsa::{RsaPrivateKey, RsaPublicKey};
+use rsa::{
+    RsaPrivateKey, RsaPublicKey,
+    pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey},
+    pkcs8::LineEnding,
+};
 use std::{path::Path, sync::LazyLock};
+
+static PRIVATE_KEY_FILENAME: &str = "private_key.pem";
+static PUBLIC_KEY_FILENAME: &str = "public_key.pem";
 
 pub fn create_certifications(dir: &Path) -> Result<(), OperationOutcomeError> {
     let mut rng = OsRng;
     let bits = 2048;
+
     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
 
-    let private_key_file = dir.join("private_key.pem");
-    let public_key_file = dir.join("public_key.pem");
+    let private_key_file = dir.join(PRIVATE_KEY_FILENAME);
+    let public_key_file = dir.join(PUBLIC_KEY_FILENAME);
+
+    std::fs::write(
+        private_key_file,
+        priv_key.to_pkcs1_pem(LineEnding::default()).unwrap(),
+    )
+    .map_err(|e| OperationOutcomeError::fatal("file_error".to_string(), e.to_string()))?;
+
+    std::fs::write(
+        public_key_file,
+        pub_key.to_pkcs1_pem(LineEnding::default()).unwrap(),
+    )
+    .map_err(|e| OperationOutcomeError::fatal("file_error".to_string(), e.to_string()))?;
+
+    Ok(())
 }
 
+static DECODING_KEY: LazyLock<jsonwebtoken::DecodingKey> = LazyLock::new(|| {
+    // let key = CERTIFICATES.public_key.clone();
+    let config = get_config(ConfigType::Environment);
+    let certificate_dir = config.get("CERTIFICATION_DIR").unwrap();
+    let cert_dir: &Path = Path::new(&certificate_dir);
+    jsonwebtoken::DecodingKey::from_rsa_pem(
+        &std::fs::read(cert_dir.join(PUBLIC_KEY_FILENAME)).unwrap(),
+    )
+    .unwrap()
+});
+
+static ENCODING_KEY: LazyLock<jsonwebtoken::EncodingKey> = LazyLock::new(|| {
+    // let key = CERTIFICATES.public_key.clone();
+    let config = get_config(ConfigType::Environment);
+    let certificate_dir = config.get("CERTIFICATION_DIR").unwrap();
+    let cert_dir: &Path = Path::new(&certificate_dir);
+    jsonwebtoken::EncodingKey::from_rsa_pem(
+        &std::fs::read(cert_dir.join(PRIVATE_KEY_FILENAME)).unwrap(),
+    )
+    .unwrap()
+});
+
 pub fn get_decoding_key() -> jsonwebtoken::DecodingKey {
-    let key = CERTIFICATES.public_key.clone();
-    jsonwebtoken::DecodingKey::from_rsa_pem(&key.to_pem().unwrap()).unwrap()
+    *DECODING_KEY
 }
