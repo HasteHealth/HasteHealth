faedb13a568856ef77e525a3fe566ceb66657518
confused-moniker <ralph@genfhi.app>
2025-08-16 20:00:15 -0500

    mod

--- crates/server/src/auth_n/certificates/signing_keys.rs
+++ crates/server/src/auth_n/certificates/signing_keys.rs
@@ -1,10 +1,19 @@
+use oxidized_fhir_operation_error::OperationOutcomeError;
 use rand::rngs::OsRng;
 use rsa::{RsaPrivateKey, RsaPublicKey};
+use std::{path::Path, sync::LazyLock};
 
-pub fn get_decoding_key() -> jsonwebtoken::DecodingKey {
+pub fn create_certifications(dir: &Path) -> Result<(), OperationOutcomeError> {
     let mut rng = OsRng;
     let bits = 2048;
     let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
-    todo!();
+
+    let private_key_file = dir.join("private_key.pem");
+    let public_key_file = dir.join("public_key.pem");
+}
+
+pub fn get_decoding_key() -> jsonwebtoken::DecodingKey {
+    let key = CERTIFICATES.public_key.clone();
+    jsonwebtoken::DecodingKey::from_rsa_pem(&key.to_pem().unwrap()).unwrap()
 }
