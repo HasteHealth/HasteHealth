ab4e260edf454fd472dc4416a565c67f2a4d50db
confused-moniker <ralph@genfhi.app>
2025-08-20 18:11:24 -0500

    mod

--- crates/server/src/auth_n/oidc/extract/client_app.rs
+++ crates/server/src/auth_n/oidc/extract/client_app.rs
@@ -28,32 +28,40 @@ pub async fn find_client_app<Repo: Repository + Send + Sync, Search: SearchEngin
     client_id: String,
 ) -> Result<ClientApplication, OperationOutcomeError> {
     let hardcoded_clients = get_hardcoded_clients(&state.config);
-    let ctx = ServerCTX {
-        tenant,
-        project,
-        fhir_version: SupportedFHIRVersions::R4,
-        author: Author {
-            id: "anonymous".to_string(),
-            kind: "Membership".to_string(),
-        },
-    };
 
-    let client_app = state
-        .fhir_client
-        .read(
-            ctx,
-            ResourceType::new("ClientApplication".to_string()).unwrap(),
-            client_id,
-        )
-        .await?;
-
-    if let Some(Resource::ClientApplication(client_app)) = client_app {
-        Ok(client_app)
+    if let Some(client) = hardcoded_clients
+        .into_iter()
+        .find(|client| client.id.as_ref() == Some(&client_id))
+    {
+        Ok(client)
     } else {
-        Err(OperationOutcomeError::error(
-            OperationOutcomeCodes::NotFound,
-            "Client application not found".to_string(),
-        ))
+        let ctx = ServerCTX {
+            tenant,
+            project,
+            fhir_version: SupportedFHIRVersions::R4,
+            author: Author {
+                id: "anonymous".to_string(),
+                kind: "Membership".to_string(),
+            },
+        };
+
+        let client_app = state
+            .fhir_client
+            .read(
+                ctx,
+                ResourceType::new("ClientApplication".to_string()).unwrap(),
+                client_id,
+            )
+            .await?;
+
+        if let Some(Resource::ClientApplication(client_app)) = client_app {
+            Ok(client_app)
+        } else {
+            Err(OperationOutcomeError::error(
+                OperationOutcomeCodes::NotFound,
+                "Client application not found".to_string(),
+            ))
+        }
     }
 }
 
