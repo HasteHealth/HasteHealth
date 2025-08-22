c34b4b16125d7498b517a9ed0a175af9577324ef
confused-moniker <ralph@genfhi.app>
2025-08-22 14:16:21 -0500

    mod

--- frontend/packages/admin-app/src/utilities.ts
+++ frontend/packages/admin-app/src/utilities.ts
@@ -1,6 +1,5 @@
 import { OperationOutcome } from "@oxidized-health/fhir-types/r4/types";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getErrorMessage(error: any): string {
   if ("response" in error) {
     const message = (error.response.body as OperationOutcome).issue
