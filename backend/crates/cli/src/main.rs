93ba1ddbd0fdd25d07cc03026f5150ef95a6273f
confused-moniker <ralph@genfhi.app>
2025-08-26 16:54:49 -0500

    mod

--- backend/crates/cli/src/main.rs
+++ backend/crates/cli/src/main.rs
@@ -73,6 +73,29 @@ fn parse_fhir_data(data: &str) -> Result<serde_json::Value, CLIError> {
     Ok(data)
 }
 
+fn format_code(rust_code: String) -> String {
+    let mut format_command = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn child process");
+
+    let mut stdin = format_command.stdin.take().expect("Failed to open stdin");
+    std::thread::spawn(move || {
+        stdin
+            .write_all(rust_code.as_bytes())
+            .expect("Failed to write to stdin");
+    });
+
+    let command_output = format_command
+        .wait_with_output()
+        .expect("Failed to read stdout");
+
+    let formatted_code = String::from_utf8_lossy(&command_output.stdout);
+
+    formatted_code.to_string()
+}
+
 fn main() -> Result<(), CLIError> {
     let cli = Cli::parse();
     match &cli.command {
@@ -88,7 +111,25 @@ fn main() -> Result<(), CLIError> {
         }
         CLICommand::Generate { command } => match command {
             CodeGen::Operations { input, output } => {
-                todo!();
+                let generated_operation_definitions =
+                    type_gen::operation_definitions::generate_operation_definitions_from_files(
+                        input,
+                    )
+                    .map_err(|e| CLIError::GenerationError(e))?;
+
+                let formatted_code = format_code(generated_operation_definitions);
+
+                match output {
+                    Some(output_path) => {
+                        std::fs::write(output_path, formatted_code.to_string())?;
+                        println!("Generated FHIR types written to: {}", output_path);
+                    }
+                    None => {
+                        println!("{}", formatted_code);
+                    }
+                }
+
+                Ok(())
             }
             CodeGen::Types {
                 input,
@@ -108,24 +149,7 @@ fn main() -> Result<(), CLIError> {
                     type_gen::fhir_rust_types::generate_fhir_types_from_files(input, level)
                         .map_err(|e| CLIError::GenerationError(e))?;
 
-                let mut format_command = Command::new("rustfmt")
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn()
-                    .expect("Failed to spawn child process");
-
-                let mut stdin = format_command.stdin.take().expect("Failed to open stdin");
-                std::thread::spawn(move || {
-                    stdin
-                        .write_all(rust_code.as_bytes())
-                        .expect("Failed to write to stdin");
-                });
-
-                let command_output = format_command
-                    .wait_with_output()
-                    .expect("Failed to read stdout");
-
-                let formatted_code = String::from_utf8_lossy(&command_output.stdout);
+                let formatted_code = format_code(rust_code);
 
                 match output {
                     Some(output_path) => {
@@ -133,7 +157,6 @@ fn main() -> Result<(), CLIError> {
                         println!("Generated FHIR types written to: {}", output_path);
                     }
                     None => {
-                        println!("Generated Rust code:");
                         println!("{}", formatted_code);
                     }
                 }
--- backend/crates/codegen/src/type_gen/operation_definitions.rs
+++ backend/crates/codegen/src/type_gen/operation_definitions.rs
@@ -1,12 +1,12 @@
 use std::{borrow::Cow, path::Path};
 
-use crate::utilities::{generate::capitalize, load};
+use crate::utilities::{FHIR_PRIMITIVES, RUST_KEYWORDS, generate::capitalize, load};
 use oxidized_fhir_model::r4::types::{OperationDefinition, OperationDefinitionParameter, Resource};
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use walkdir::WalkDir;
 
-pub fn get_operation_definitions<'a>(
+fn get_operation_definitions<'a>(
     resource: &'a Resource,
 ) -> Result<Vec<&'a OperationDefinition>, String> {
     match resource {
@@ -47,53 +47,114 @@ fn get_name(op_def: &OperationDefinition) -> String {
     interface_name
 }
 
-fn generate_parameter_type(parameters: &Vec<&OperationDefinitionParameter>) -> TokenStream {
-    let types = vec![];
+fn create_field_value(type_: &str, is_array: bool, required: bool) -> TokenStream {
+    let type_ = if let Some(primitive) = FHIR_PRIMITIVES.get(type_) {
+        primitive.as_str()
+    } else {
+        type_
+    };
+
+    let type_ = format_ident!("{}", type_);
+
+    let type_ = if is_array {
+        quote! {Vec<#type_>}
+    } else {
+        quote! {#type_}
+    };
+
+    let type_ = if required {
+        quote! { #type_ }
+    } else {
+        quote! {Option<#type_>}
+    };
+
+    type_
+}
+
+fn generate_parameter_type(
+    name: &str,
+    parameters: &Vec<&OperationDefinitionParameter>,
+) -> Vec<TokenStream> {
+    let mut types = vec![];
+    let mut fields = vec![];
+
     for p in parameters.iter() {
         let is_array = p.max.value != Some("1".to_string());
         let required = p.min.value.unwrap_or(0) > 0;
-        let field_name = p.name.value.as_ref().expect("Parameter must have a name");
+        let field_name = p
+            .name
+            .value
+            .as_ref()
+            .expect("Parameter must have a name")
+            .replace("-", "_");
+        let field_ident = if RUST_KEYWORDS.contains(&field_name.as_str()) {
+            format_ident!("{}_", field_name)
+        } else {
+            format_ident!("{}", field_name)
+        };
 
         if let Some(type_) = p.type_.as_ref().and_then(|v| v.value.as_ref()) {
+            let type_ = if type_ == "Any" { "Resource" } else { type_ };
+            let type_ = create_field_value(type_, is_array, required);
+
+            fields.push(quote! {pub #field_ident: #type_ })
         } else {
+            let name = name.to_string() + &capitalize(field_name.as_str());
+            let nested_types = generate_parameter_type(
+                &name,
+                &p.part
+                    .as_ref()
+                    .map(|v| v.iter().collect())
+                    .unwrap_or(vec![]),
+            );
+            types.extend(nested_types);
+
+            let type_ = create_field_value(&name, is_array, required);
+            fields.push(quote! {pub #field_ident: #type_ })
         }
     }
 
-    panic!();
+    let struct_name = format_ident!("{}", name);
 
-    //   const fields = parameters
-    //     .map((p) => {
-    //       const isArray = p.max !== "1";
-    //       const required = p.min > 0;
+    let base_parameter_type = quote! {
+        pub struct #struct_name {
+            #(#fields),*
+        }
+    };
 
-    //       const fieldName = `"${p.name}"${required ? "" : "?"}`;
-    //       const type = p.type === "Any" ? "Resource" : p.type;
+    types.push(base_parameter_type);
 
-    //       const singularValue = p.type
-    //         ? `fhirTypes.${type}`
-    //         : generateParameterType(p.part || []);
-    //       const value = isArray ? `Array<${singularValue}>` : singularValue;
-
-    //       return `${fieldName}: ${value}`;
-    //     })
-    //     .join(",\n");
-    //   return `{${fields}}`;
+    types
 }
 
-fn generate_output(parameters: &Cow<Vec<OperationDefinitionParameter>>) -> TokenStream {}
-
-fn generate_input(parameters: &Cow<Vec<OperationDefinitionParameter>>) -> TokenStream {
+fn generate_output(parameters: &Cow<Vec<OperationDefinitionParameter>>) -> Vec<TokenStream> {
     let input_parameters = parameters
         .iter()
         .filter(|p| {
-            p.use_value
+            p.use_
+                .value
+                .as_ref()
+                .map(|u| u.as_str() == "out")
+                .unwrap_or(false)
+        })
+        .collect::<Vec<_>>();
+
+    generate_parameter_type("Output", &input_parameters)
+}
+
+fn generate_input(parameters: &Cow<Vec<OperationDefinitionParameter>>) -> Vec<TokenStream> {
+    let input_parameters = parameters
+        .iter()
+        .filter(|p| {
+            p.use_
+                .value
                 .as_ref()
                 .map(|u| u.as_str() == "in")
                 .unwrap_or(false)
         })
         .collect::<Vec<_>>();
 
-    generate_parameter_type(&input_parameters)
+    generate_parameter_type("Input", &input_parameters)
 }
 
 fn generate_operation_definition(file_path: &Path) -> Result<TokenStream, String> {
@@ -102,7 +163,7 @@ fn generate_operation_definition(file_path: &Path) -> Result<TokenStream, String
     // Generate code for each operation definition
     let mut generated = quote! {};
     for op_def in op_defs {
-        let name = get_name(op_def);
+        let name = format_ident!("{}", get_name(op_def));
         let parameters = op_def
             .parameter
             .as_ref()
@@ -113,9 +174,10 @@ fn generate_operation_definition(file_path: &Path) -> Result<TokenStream, String
         let generate_output = generate_output(&parameters);
 
         generated.extend(quote! {
-            mod #name {
-                #generate_input
-                #generate_output
+            pub mod #name {
+                use super::*;
+                #(#generate_input)*
+                #(#generate_output)*
             }
             // Code generation for each operation definition
         });
@@ -124,17 +186,13 @@ fn generate_operation_definition(file_path: &Path) -> Result<TokenStream, String
     Ok(generated)
 }
 
-pub fn generate_operation_definitions(file_paths: &Vec<String>) -> Result<String, String> {
+pub fn generate_operation_definitions_from_files(
+    file_paths: &Vec<String>,
+) -> Result<String, String> {
     let mut generated_code = quote! {
         #![allow(non_snake_case)]
-        use oxidized_reflect::{MetaValue, derive::Reflect};
-        use oxidized_fhir_serialization_json;
-        use oxidized_fhir_serialization_json::FHIRJSONDeserializer;
-        use thiserror::Error;
-        use std::io::Write;
+        use oxidized_fhir_model::r4::types::*;
     };
-
-    let mut resource_types: Vec<String> = vec![];
 
     for dir_path in file_paths {
         let walker = WalkDir::new(dir_path).into_iter();
