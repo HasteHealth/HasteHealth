48fc048ff45fba0030a44af5f82ec133a82fc971
confused-moniker <ralph@genfhi.app>
2025-08-27 11:59:03 -0500

    mod

--- backend/crates/fhir-terminology/src/client.rs
+++ backend/crates/fhir-terminology/src/client.rs
@@ -1,14 +1,14 @@
-use crate::{FHIRTerminology, TerminologyError};
+use crate::FHIRTerminology;
 use oxidized_fhir_client::FHIRClient;
 use oxidized_fhir_generated_ops::{CodeSystemLookup, ValueSetExpand, ValueSetValidateCode};
 use oxidized_fhir_model::r4::types::ResourceType;
 use oxidized_fhir_operation_error::OperationOutcomeError;
-use std::{marker::PhantomData, sync::Arc};
+use std::marker::PhantomData;
 
 pub struct FHIRClientTerminology<CTX, Error, Client: FHIRClient<CTX, Error>> {
     _ctx: PhantomData<CTX>,
     _error: PhantomData<Error>,
-    client: Arc<Box<Client>>,
+    client: Box<Client>,
 }
 
 impl<CTX: Send + Sync, Client: FHIRClient<CTX, OperationOutcomeError>> FHIRTerminology<CTX>
@@ -29,7 +29,7 @@ impl<CTX: Send + Sync, Client: FHIRClient<CTX, OperationOutcomeError>> FHIRTermi
         &self,
         ctx: CTX,
         input: &ValueSetValidateCode::Input,
-    ) -> Result<ValueSetValidateCode::Output, TermiOperationOutcomeErrornologyError> {
+    ) -> Result<ValueSetValidateCode::Output, OperationOutcomeError> {
         // Implementation would go here
         unimplemented!()
     }
