6fbbb5312f43ea2752ad94956f7416320d226175
confused-moniker <93403502+confused-moniker@users.noreply.github.com>
2025-08-29 13:19:03 -0500

    Not Modifier (#112)

--- backend/crates/fhir-search/src/elastic_search/search/clauses/token.rs
+++ backend/crates/fhir-search/src/elastic_search/search/clauses/token.rs
@@ -3,11 +3,20 @@ use oxidized_fhir_client::url::Parameter;
 use oxidized_fhir_model::r4::types::SearchParameter;
 use serde_json::json;
 
+fn matching_modifier(modifier: &Option<String>) -> Result<String, QueryBuildError> {
+    match modifier.as_ref().map(|s| s.as_str()) {
+        Some("not") => Ok("must_not".to_string()),
+        Some(modifier) => Err(QueryBuildError::ModifierNotSupported(modifier.into())),
+        None => Ok("must".to_string()),
+    }
+}
+
 pub fn token(
-    parsed_parameter: &Parameter,
+    parameter: &Parameter,
     search_param: &SearchParameter,
 ) -> Result<serde_json::Value, QueryBuildError> {
-    let params = parsed_parameter
+    let matching_type = matching_modifier(&parameter.modifier)?;
+    let params = parameter
         .value
         .iter()
         .map(|value| {
@@ -18,10 +27,13 @@ pub fn token(
                         "nested": {
                             "path": search_param.url.value.as_ref().unwrap(),
                             "query": {
-                                "match": {
-                                    search_param.url.value.as_ref().unwrap().to_string() + ".code": {
-                                    "query": pieces.get(0)
-                                    }
+                                "bool": {
+                                    matching_type.clone(): [{
+                                        "match": {
+                                            search_param.url.value.as_ref().unwrap().to_string() + ".code": {
+                                            "query": pieces.get(0)
+                                        }
+                                    }}]
                                 }
                             }
                         }
@@ -33,7 +45,7 @@ pub fn token(
                             "path": search_param.url.value.as_ref().unwrap(),
                             "query": {
                                 "bool": {
-                                    "must": [
+                                    matching_type.clone(): [
                                         {
                                             "match": {
                                                 search_param.url.value.as_ref().unwrap().to_string() + ".code": {
