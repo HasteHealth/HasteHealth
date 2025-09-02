39ef2474fff2b2fd4f94c45fa5ac92f6c0462456
confused-moniker <ralph@genfhi.app>
2025-09-01 20:09:59 -0500

    mod framework

--- backend/crates/fhir-ops/src/lib.rs
+++ backend/crates/fhir-ops/src/lib.rs
@@ -1,1 +1,21 @@
+use oxidized_fhir_model::r4::types::Parameters;
+use oxidized_fhir_operation_error::OperationOutcomeError;
 
+pub enum Param<I: TryFrom<Parameters, Error = OperationOutcomeError>> {
+    Value(I),
+    Parameters(Parameters),
+}
+
+pub trait OperationInvocation<
+    I: TryFrom<Parameters, Error = OperationOutcomeError>,
+    Output: TryFrom<Parameters, Error = OperationOutcomeError>,
+>
+{
+    fn new(
+        executor: Box<
+            dyn Fn(Param<I>) -> Result<Param<Output>, OperationOutcomeError> + Send + Sync,
+        >,
+    ) -> Self;
+
+    fn execute(&self, input: Param<I>) -> Result<Param<Output>, OperationOutcomeError>;
+}
