c86715ec62344e13e829e37ab804a761895c7e7e
confused-moniker <ralph@genfhi.app>
2025-09-05 11:06:16 -0500

    mod

--- backend/crates/fhir-terminology/src/client.rs
+++ backend/crates/fhir-terminology/src/client.rs
@@ -1,19 +1,56 @@
 use crate::FHIRTerminology;
 use oxidized_fhir_client::{
-    FHIRClient,
-    // url::{Parameter, ParsedParameter},
+    FHIRClient, // url::{Parameter, ParsedParameter},
+    url::{Parameter, ParsedParameter},
 };
 use oxidized_fhir_generated_ops::generated::{
     CodeSystemLookup, ValueSetExpand, ValueSetValidateCode,
 };
+use oxidized_fhir_model::r4::types::{Resource, ResourceType, ValueSet};
 // use oxidized_fhir_model::r4::types::ResourceType;
-use oxidized_fhir_operation_error::OperationOutcomeError;
-use std::marker::PhantomData;
+use oxidized_fhir_operation_error::{OperationOutcomeCodes, OperationOutcomeError};
+use std::{marker::PhantomData, sync::Arc};
 
 pub struct FHIRClientTerminology<CTX, Error, Client: FHIRClient<CTX, Error>> {
     _ctx: PhantomData<CTX>,
     _error: PhantomData<Error>,
-    _client: Box<Client>,
+    client: Arc<Box<Client>>,
+}
+
+async fn resolve_valueset<CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
+    client: Arc<Box<Client>>,
+    ctx: CTX,
+    input: &ValueSetExpand::Input,
+) -> Result<Option<ValueSet>, OperationOutcomeError> {
+    if let Some(valueset) = input.valueSet {
+        return Ok(Some(valueset.clone()));
+    } else if let Some(url) = &input.url.and_then(|u| u.value.as_ref()) {
+        let mut result = client
+            .search_type(
+                ctx,
+                ResourceType::ValueSet,
+                vec![ParsedParameter::Resource(Parameter {
+                    name: "url".to_string(),
+                    value: vec![url.to_string()],
+                    modifier: None,
+                    chains: None,
+                })],
+            )
+            .await?;
+        if result.len() > 1 {
+            return Err(OperationOutcomeError::error(
+                OperationOutcomeCodes::Duplicate,
+                format!("Multiple ValueSet resources found for url {}", url),
+            ));
+        } else if let Some(resource) = result.pop() {
+            return match resource {
+                Resource::ValueSet(vs) => Ok(Some(vs)),
+                _ => Ok(None),
+            };
+        }
+    }
+
+    Ok(None)
 }
 
 impl<CTX: Send + Sync, Client: FHIRClient<CTX, OperationOutcomeError>> FHIRTerminology<CTX>
@@ -21,23 +58,23 @@ impl<CTX: Send + Sync, Client: FHIRClient<CTX, OperationOutcomeError>> FHIRTermi
 {
     async fn expand(
         &self,
-        _ctx: CTX,
-        _input: &ValueSetExpand::Input,
+        ctx: CTX,
+        input: &ValueSetExpand::Input,
     ) -> Result<ValueSetExpand::Output, OperationOutcomeError> {
         // Implementation would go here
-        // let _result = self
-        //     .client
-        //     .search_type(
-        //         ctx,
-        //         ResourceType::ValueSet,
-        //         vec![ParsedParameter::Resource(Parameter {
-        //             name: "url".to_string(),
-        //             value: vec!["http://example.org/fhir/ValueSet/example".to_string()],
-        //             modifier: None,
-        //             chains: None,
-        //         })],
-        //     )
-        //     .await;
+        let _result = self
+            .client
+            .search_type(
+                ctx,
+                ResourceType::ValueSet,
+                vec![ParsedParameter::Resource(Parameter {
+                    name: "url".to_string(),
+                    value: vec!["http://example.org/fhir/ValueSet/example".to_string()],
+                    modifier: None,
+                    chains: None,
+                })],
+            )
+            .await;
 
         unimplemented!();
     }
