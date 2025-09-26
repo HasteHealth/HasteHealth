6f039d2eef0d01abf2859f7a0172863ddf01256d
confused-moniker <ralph@genfhi.app>
2025-09-26 14:10:20 -0500

    mod

--- backend/crates/fhir-generated-ops/src/lib.rs
+++ backend/crates/fhir-generated-ops/src/lib.rs
@@ -14,7 +14,7 @@ mod tests {
         let result = generated::ActivityDefinitionApply::Output::try_from(vec![
             oxidized_fhir_model::r4::generated::resources::ParametersParameter {
                 name: Box::new(FHIRString {
-                    value: Some("return_".to_string()),
+                    value: Some("return".to_string()),
                     ..Default::default()
                 }),
                 resource: Some(Box::new(Resource::Patient(Patient {
