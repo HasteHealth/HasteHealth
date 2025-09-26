9ab8b6d6ab34421c6d6898444f8ff0f110b4d1f1
confused-moniker <ralph@genfhi.app>
2025-09-26 16:25:49 -0500

    mod

--- backend/crates/fhir-ops/src/lib.rs
+++ backend/crates/fhir-ops/src/lib.rs
@@ -1,24 +1,90 @@
-use oxidized_fhir_model::r4::generated::resources::Parameters;
+use std::pin::Pin;
+
+use oxidized_fhir_model::r4::generated::resources::{Parameters, ParametersParameter};
 use oxidized_fhir_operation_error::OperationOutcomeError;
 
 #[cfg(feature = "derive")]
 pub mod derive;
 
-pub enum Param<I: TryFrom<Parameters, Error = OperationOutcomeError>> {
-    Value(I),
+pub enum Param<
+    T: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>,
+> {
+    Value(T),
     Parameters(Parameters),
 }
 
 pub trait OperationInvocation<
-    I: TryFrom<Parameters, Error = OperationOutcomeError>,
-    Output: TryFrom<Parameters, Error = OperationOutcomeError>,
+    I: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>,
+    O: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>,
 >
 {
-    fn new(
-        executor: Box<
-            dyn Fn(Param<I>) -> Result<Param<Output>, OperationOutcomeError> + Send + Sync,
-        >,
-    ) -> Self;
+    fn execute(
+        &self,
+        input: Param<I>,
+    ) -> impl Future<Output = Result<Param<O>, OperationOutcomeError>> + Send + Sync;
+}
 
-    fn execute(&self, input: Param<I>) -> Result<Param<Output>, OperationOutcomeError>;
+pub struct OperationExecutor<
+    I: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+    O: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+> {
+    executor: Box<
+        dyn Fn(
+                Param<I>,
+            ) -> Pin<
+                Box<dyn Future<Output = Result<Param<O>, OperationOutcomeError>> + Send + Sync>,
+            > + Send
+            + Sync,
+    >,
+}
+
+impl<
+    I: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+    O: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+> OperationExecutor<I, O>
+{
+    pub fn new(
+        executor: Box<
+            dyn Fn(
+                    Param<I>,
+                ) -> Pin<
+                    Box<dyn Future<Output = Result<Param<O>, OperationOutcomeError>> + Send + Sync>,
+                > + Send
+                + Sync,
+        >,
+    ) -> Self {
+        Self { executor }
+    }
+}
+
+impl<
+    I: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+    O: TryFrom<Vec<ParametersParameter>, Error = OperationOutcomeError>
+        + Into<Vec<ParametersParameter>>
+        + Send
+        + Sync,
+> OperationInvocation<I, O> for OperationExecutor<I, O>
+{
+    async fn execute(&self, input: Param<I>) -> Result<Param<O>, OperationOutcomeError> {
+        let output = (self.executor)(input).await;
+        output
+    }
 }
