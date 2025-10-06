e06cf32e1f23c00ae4dfc79c344940038dbf1f27
confused-moniker <ralph@genfhi.app>
2025-10-06 16:38:23 -0500

    sql

--- backend/crates/server/migrations/20251006212231_membership_table.sql
+++ backend/crates/server/migrations/20251006212231_membership_table.sql
@@ -2,11 +2,13 @@ CREATE TYPE membership_role AS ENUM ('admin', 'member');
 
 CREATE TABLE
     memberships (
-        tenant text NOT NULL,
-        user text NOT NULL,
+        tenant TEXT NOT NULL,
+        project TEXT NOT NULL,
+        user_id TEXT NOT NULL,
         role membership_role NOT NULL,
         updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        CONSTRAINT membership_pkey PRIMARY KEY (tenant, user),
-        CONSTRAINT fk_user FOREIGN KEY (user) REFERENCES users (fhir_user_id) ON DELETE CASCADE,
-        CONSTRAINT fk_tenant FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE
+        CONSTRAINT membership_pkey PRIMARY KEY (tenant, user_id),
+        CONSTRAINT fk_tenant FOREIGN KEY (tenant) REFERENCES tenants (id) ON DELETE CASCADE,
+        CONSTRAINT fk_project FOREIGN KEY (tenant, project) REFERENCES project (tenant, id) ON DELETE CASCADE,
+        CONSTRAINT fk_user FOREIGN KEY (tenant, user_id) REFERENCES users (tenant, id) ON DELETE CASCADE
     );
