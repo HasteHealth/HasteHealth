3ce870bad00b0b4b4f576f09dc82d384dc4674f9
confused-moniker <ralph@genfhi.app>
2025-10-07 14:45:56 -0500

    mod

--- backend/crates/server/src/fhir_client/middleware/membership.rs
+++ backend/crates/server/src/fhir_client/middleware/membership.rs
@@ -1,15 +1,21 @@
-use crate::fhir_client::{
-    ClientState, ServerCTX,
-    middleware::{
-        ServerMiddlewareContext, ServerMiddlewareNext, ServerMiddlewareOutput,
-        ServerMiddlewareState,
+use crate::{
+    auth_n::session::user::get_user,
+    fhir_client::{
+        ClientState, ServerCTX,
+        middleware::{
+            ServerMiddlewareContext, ServerMiddlewareNext, ServerMiddlewareOutput,
+            ServerMiddlewareState,
+        },
     },
 };
 use oxidized_fhir_client::{
     middleware::MiddlewareChain,
     request::{FHIRRequest, FHIRResponse},
 };
-use oxidized_fhir_model::r4::generated::terminology::IssueType;
+use oxidized_fhir_model::r4::generated::{
+    resources::{Membership, Resource},
+    terminology::IssueType,
+};
 use oxidized_fhir_operation_error::OperationOutcomeError;
 use oxidized_fhir_search::SearchEngine;
 use oxidized_fhir_terminology::FHIRTerminology;
@@ -42,6 +48,15 @@ async fn setup_transaction_context<
             "Request type not supported for membership middleware.".to_string(),
         )),
     }
+}
+
+fn get_user_id<'a>(membership: &'a Membership) -> Option<&'a str> {
+    if let Some(user_reference) = membership.user.reference.and_then(|r| r.value)
+        && let Some(user_id) = user_reference.split('/').last()
+    {
+        Some(user_id)
+    }
+    None
 }
 
 pub struct MembershipTableAlterationMiddleware {}
@@ -81,24 +96,78 @@ impl<
 
                     let res = next(transaction_state.clone(), context).await?;
 
-                    match &context.request {
-                        FHIRRequest::Create(_) => {
-                            let k = ProjectAuthAdmin::create(
-                                repo_client.as_ref(),
-                                &context.ctx.tenant,
-                                &context.ctx.project,
-                                m::CreateMembership {
-                                    role: m::MembershipRole::Member,
-                                    user_id: "asdf".to_string(),
-                                },
-                            )
-                            .await?;
+                    match res.response.as_ref() {
+                        Some(FHIRResponse::Create(create_response)) => {
+                            if let Some(user_id) = get_user_id(membership) {
+                                let k = ProjectAuthAdmin::create(
+                                    repo_client.as_ref(),
+                                    &context.ctx.tenant,
+                                    &context.ctx.project,
+                                    m::CreateMembership {
+                                        role: m::MembershipRole::Member,
+                                        user_id: user_id,
+                                    },
+                                )
+                                .await?;
 
-                            Ok(())
+                                Ok(())
+                            }
                         }
-                        FHIRRequest::DeleteInstance(_)
-                        | FHIRRequest::UpdateInstance(_)
-                        | FHIRRequest::ConditionalUpdate(_) => Ok(()),
+                        FHIRResponse::DeleteInstance(delete_response) => {
+                            if let Some(user_id) = get_user_id(membership) {
+                                let k = ProjectAuthAdmin::delete(
+                                    repo_client.as_ref(),
+                                    &context.ctx.tenant,
+                                    &context.ctx.project,
+                                    user_id,
+                                )
+                                .await?;
+
+                                Ok(())
+                            }
+                        }
+                        FHIRResponse::UpdateInstance(update_response) => {
+                            if let Some(user_id) = get_user_id(membership) {
+                                let k = ProjectAuthAdmin::update(
+                                    repo_client.as_ref(),
+                                    &context.ctx.tenant,
+                                    &context.ctx.project,
+                                    m::Membership {
+                                        role: membership
+                                            .role
+                                            .clone()
+                                            .unwrap_or(m::MembershipRole::Member),
+                                        user_id: user_id.to_string(),
+                                        tenant: context.ctx.tenant.clone(),
+                                        project: context.ctx.project.clone(),
+                                    },
+                                )
+                                .await?;
+
+                                Ok(())
+                            }
+                        }
+                        FHIRResponse::ConditionalUpdate(_) => {
+                            if let Some(user_id) = get_user_id(membership) {
+                                let k = ProjectAuthAdmin::update(
+                                    repo_client.as_ref(),
+                                    &context.ctx.tenant,
+                                    &context.ctx.project,
+                                    m::Membership {
+                                        role: membership
+                                            .role
+                                            .clone()
+                                            .unwrap_or(m::MembershipRole::Member),
+                                        user_id: user_id.to_string(),
+                                        tenant: context.ctx.tenant.clone(),
+                                        project: context.ctx.project.clone(),
+                                    },
+                                )
+                                .await?;
+
+                                Ok(())
+                            }
+                        }
                         _ => Ok(()),
                     }?;
 
