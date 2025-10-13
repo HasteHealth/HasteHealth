b405b4099ae9da4c03a8c52a73dc2a9d3978811a
confused-moniker <ralph@genfhi.app>
2025-10-13 16:51:42 -0500

    all arms

--- backend/crates/server/src/fhir_client/utilities.rs
+++ backend/crates/server/src/fhir_client/utilities.rs
@@ -4,22 +4,27 @@ use oxidized_fhir_model::r4::generated::resources::ResourceType;
 /// Converts a FHIRRequest to its corresponding ResourceType if applicable.
 pub fn request_to_resource_type<'a>(request: &'a FHIRRequest) -> Option<&'a ResourceType> {
     match request {
-        // Instance operations
         FHIRRequest::Read(req) => Some(&req.resource_type),
         FHIRRequest::VersionRead(req) => Some(&req.resource_type),
         FHIRRequest::UpdateInstance(req) => Some(&req.resource_type),
         FHIRRequest::DeleteInstance(req) => Some(&req.resource_type),
         FHIRRequest::Patch(req) => Some(&req.resource_type),
         FHIRRequest::HistoryInstance(req) => Some(&req.resource_type),
+        FHIRRequest::InvokeInstance(req) => Some(&req.resource_type),
 
-        // Type operations
         FHIRRequest::Create(req) => Some(&req.resource_type),
         FHIRRequest::HistoryType(req) => Some(&req.resource_type),
         FHIRRequest::SearchType(req) => Some(&req.resource_type),
         FHIRRequest::ConditionalUpdate(req) => Some(&req.resource_type),
         FHIRRequest::DeleteType(req) => Some(&req.resource_type),
+        FHIRRequest::InvokeType(req) => Some(&req.resource_type),
 
-        // System operations default to None.
-        _ => None,
+        FHIRRequest::HistorySystem(_)
+        | FHIRRequest::DeleteSystem(_)
+        | FHIRRequest::Capabilities
+        | FHIRRequest::SearchSystem(_)
+        | &FHIRRequest::InvokeSystem(_)
+        | FHIRRequest::Batch(_)
+        | FHIRRequest::Transaction(_) => None,
     }
 }
