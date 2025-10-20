c053b46aeacd480fdc7680028b242a3a77711562
confused-moniker <ralph@genfhi.app>
2025-10-20 16:49:28 -0500

    mod

--- backend/crates/server/src/auth_n/oidc/routes/route_string.rs
+++ backend/crates/server/src/auth_n/oidc/routes/route_string.rs
@@ -5,9 +5,9 @@ pub fn tenant_route_string(tenant: &TenantId) -> String {
 }
 
 pub fn project_route_string(tenant: &TenantId, project: &ProjectId) -> String {
-    format!("/{}/{}", tenant_route_string(tenant), project.as_ref())
+    format!("{}/{}", tenant_route_string(tenant), project.as_ref())
 }
 
 pub fn oidc_route_string(tenant: &TenantId, project: &ProjectId, path: &str) -> String {
-    format!("/{}/oidc/{}", project_route_string(tenant, project), path)
+    format!("{}/oidc/{}", project_route_string(tenant, project), path)
 }
