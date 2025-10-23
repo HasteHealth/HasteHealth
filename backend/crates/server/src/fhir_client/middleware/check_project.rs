1f4bd95e899e98e631e0e069ad276fea5615bc9d
confused-moniker <ralph@genfhi.app>
2025-10-23 12:31:31 -0500

    readonly

--- backend/crates/server/src/fhir_client/middleware/check_project.rs
+++ backend/crates/server/src/fhir_client/middleware/check_project.rs
@@ -23,7 +23,7 @@ pub struct Middleware {
 }
 impl Middleware {
     pub fn new(project_id: ProjectId) -> Self {
-        Middleware { project_id }
+        Self { project_id }
     }
 }
 impl<
@@ -58,6 +58,61 @@ impl<
                         "Must be in project {} to access this resource, not {}",
                         project_id, context.ctx.project,
                     ),
+                ))
+            }
+        })
+    }
+}
+
+pub struct SetProjectReadOnlyMiddleware {
+    project_id: ProjectId,
+}
+impl SetProjectReadOnlyMiddleware {
+    pub fn new(project_id: ProjectId) -> Self {
+        Self { project_id }
+    }
+}
+impl<
+    Repo: Repository + Send + Sync + 'static,
+    Search: SearchEngine + Send + Sync + 'static,
+    Terminology: FHIRTerminology + Send + Sync + 'static,
+>
+    MiddlewareChain<
+        ServerMiddlewareState<Repo, Search, Terminology>,
+        Arc<ServerCTX>,
+        FHIRRequest,
+        FHIRResponse,
+        OperationOutcomeError,
+    > for SetProjectReadOnlyMiddleware
+{
+    fn call(
+        &self,
+        state: ServerMiddlewareState<Repo, Search, Terminology>,
+        mut context: ServerMiddlewareContext,
+        next: Option<Arc<ServerMiddlewareNext<Repo, Search, Terminology>>>,
+    ) -> ServerMiddlewareOutput {
+        let project_id = self.project_id.clone();
+        Box::pin(async move {
+            if let Some(next) = next {
+                match &context.request {
+                    FHIRRequest::Read(_)
+                    | FHIRRequest::VersionRead(_)
+                    | FHIRRequest::SearchSystem(_)
+                    | FHIRRequest::SearchType(_) => {
+                        context.ctx = Arc::new(ServerCTX {
+                            tenant: context.ctx.tenant.clone(),
+                            project: ProjectId::System,
+                            fhir_version: context.ctx.fhir_version.clone(),
+                            author: context.ctx.author.clone(),
+                        });
+                        next(state, context).await
+                    }
+                    _ => next(state, context).await,
+                }
+            } else {
+                Err(OperationOutcomeError::fatal(
+                    IssueType::Exception(None),
+                    "No next middleware found".to_string(),
                 ))
             }
         })
