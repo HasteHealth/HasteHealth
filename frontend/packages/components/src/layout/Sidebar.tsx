134b51d9f30b4c888f456134ec5539be5070fdc8
confused-moniker <ralph@genfhi.app>
2025-10-28 01:19:44 -0500

    sidebar alt

--- frontend/packages/components/src/layout/Sidebar.tsx
+++ frontend/packages/components/src/layout/Sidebar.tsx
@@ -23,8 +23,8 @@ export function SideBarItem({
         className={classNames(
           "cursor-pointer flex items-center p-1 px-2 group rounded-lg group",
           {
-            "text-slate-800 hover:bg-gray-100": !active,
-            "text-teal-800 bg-teal-100 ": active,
+            "text-slate-800 hover:bg-slate-200": !active,
+            "text-teal-900 bg-teal-200 ": active,
           }
         )}
       >
@@ -50,7 +50,7 @@ export interface SideBarItemGroupProps
 export function SideBarItemGroup(props: SideBarItemGroupProps) {
   return (
     <li {...props}>
-      <div className="px-2 text-teal-800 text-xs">{props.label}</div>
+      <div className="px-2 text-teal-900 text-xs">{props.label}</div>
       <div className="mt-1 ml-1">
         <ul className="space-y-1">{props.children}</ul>
       </div>
