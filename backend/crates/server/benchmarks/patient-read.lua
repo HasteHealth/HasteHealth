ebfa5e6c2f2e735695b9c2aa0c6757c8ebbfa260
RP <93403502+confused-moniker@users.noreply.github.com>
2025-11-02 16:43:32 -0600

    mod (#215)

--- backend/crates/server/benchmarks/patient-read.lua
+++ backend/crates/server/benchmarks/patient-read.lua
@@ -1,4 +1,3 @@
 wrk.host    = "localhost"
-wrk.path   = "/w/test-system/api/v1/project/fhir/r4/Patient/3ryUU7I1Wyo6CSHY33-n9w7PqT"
 wrk.method = "GET"
 wrk.headers["Content-Type"] = "application/json"
