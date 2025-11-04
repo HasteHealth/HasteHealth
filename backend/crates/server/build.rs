3bec846a5560e4ceedb6526ef03ef161d1d9a2b7
confused-moniker <ralph@genfhi.app>
2025-11-04 13:17:27 -0600

    mod

--- backend/crates/server/build.rs
+++ backend/crates/server/build.rs
@@ -1,11 +1,23 @@
 fn main() {
     println!("cargo::rerun-if-changed=css");
     println!("cargo::rerun-if-changed=src");
-    let mut tailwindcss_process = std::process::Command::new("./tools/tailwindcss")
-        .args(["-i", "./css/app.css"])
-        .args(["-o", "./public/css/app.css"])
-        .spawn()
-        .expect("Failed to spawn child process");
+
+    let mut tailwindcss_process = match std::env::consts::OS {
+        "macos" => std::process::Command::new("./tools/tailwindcss-macos")
+            .args(["-i", "./css/app.css"])
+            .args(["-o", "./public/css/app.css"])
+            .spawn()
+            .expect("Failed to spawn child process"),
+        "windows" => panic!("Unsupported OS"),
+        "linux" => {
+             std::process::Command::new("./tools/tailwindcss-linux-x64")
+            .args(["-i", "./css/app.css"])
+            .args(["-o", "./public/css/app.css"])
+            .spawn()
+            .expect("Failed to spawn child process"),
+        }
+        _ => panic!("Unsupported OS"),
+    };
 
     let result = tailwindcss_process.wait().expect("TAILWIND FAILED");
 
