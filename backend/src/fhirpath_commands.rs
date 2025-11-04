230a96fd32c65ae843a213a3a61ba5c0e893b464
confused-moniker <ralph@genfhi.app>
2025-11-03 19:40:52 -0600

    cleanup

---
+++ backend/src/fhirpath_commands.rs
@@ -0,0 +1,40 @@
+use oxidized_fhir_model::r4::generated::{resources::Resource, terminology::IssueType};
+use oxidized_fhir_operation_error::OperationOutcomeError;
+
+fn parse_fhir_data() -> Result<Resource, OperationOutcomeError> {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).map_err(|_| {
+        OperationOutcomeError::fatal(
+            IssueType::Exception(None),
+            "Failed to read from stdin.".into(),
+        )
+    })?;
+    let resource =
+        oxidized_fhir_serialization_json::from_str::<Resource>(&buffer).map_err(|e| {
+            OperationOutcomeError::error(
+                IssueType::Exception(None),
+                format!(
+                    "Failed to parse FHIR data must be a FHIR R4 Resource: {}",
+                    e
+                ),
+            )
+        })?;
+
+    Ok(resource)
+}
+
+pub fn fhirpath(fhirpath: &str) -> Result<(), OperationOutcomeError> {
+    let data = parse_fhir_data()?;
+    let engine = oxidized_fhirpath::FPEngine::new();
+
+    let result = engine.evaluate(fhirpath, vec![&data]).map_err(|e| {
+        OperationOutcomeError::error(
+            IssueType::Exception(None),
+            format!("Failed to evaluate FHIRPath: {}", e),
+        )
+    })?;
+
+    println!("{:#?}", result.iter().collect::<Vec<_>>());
+
+    Ok(())
+}
