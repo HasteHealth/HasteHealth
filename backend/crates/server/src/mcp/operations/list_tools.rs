25a2b7e21207a871d3d635ea8da641be87bad16d
confused-moniker <ralph@iguhealth.app>
2025-12-04 16:43:32 -0600

    mod

--- backend/crates/server/src/mcp/operations/list_tools.rs
+++ backend/crates/server/src/mcp/operations/list_tools.rs
@@ -20,25 +20,37 @@ pub async fn list_tools<
     ctx: Arc<ServerCTX<Repo, Search, Terminology>>,
     _request: &ListToolsRequest,
 ) -> Result<ListToolsResult, MCPError<serde_json::Value>> {
-    let _capabilities = ctx.client.capabilities(ctx.clone()).await?;
+    let capabilities = ctx.client.capabilities(ctx.clone()).await?;
+
+    let resource_tools = capabilities
+        .rest
+        .unwrap_or_default()
+        .into_iter()
+        .map(|r| r.resource.unwrap_or_default())
+        .flatten()
+        .map(|resource| {
+            let type_: Option<String> = resource.type_.as_ref().into();
+
+            Tool {
+                annotations: None,
+                description: resource.profile.and_then(|p| p.value),
+                execution: None,
+                icons: vec![],
+                input_schema: ToolInputSchema {
+                    properties: HashMap::new(),
+                    required: vec![],
+                    schema: None,
+                    type_: "object".to_string(),
+                },
+                meta: None,
+                name: type_.clone().unwrap_or("Unknown".to_string()),
+                output_schema: None,
+                title: type_,
+            }
+        });
 
     Ok(ListToolsResult {
-        tools: vec![Tool {
-            annotations: None,
-            description: None,
-            execution: None,
-            icons: vec![],
-            input_schema: ToolInputSchema {
-                properties: HashMap::new(),
-                required: vec![],
-                schema: None,
-                type_: "object".to_string(),
-            },
-            meta: None,
-            name: "Tool".to_string(),
-            output_schema: None,
-            title: Some("Tool".to_string()),
-        }],
+        tools: resource_tools.collect(),
         meta: None,
         next_cursor: None,
     })
