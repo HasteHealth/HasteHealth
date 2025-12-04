a5e6a65029236fc3bc4bd51b6534fb27de28e65d
confused-moniker <ralph@iguhealth.app>
2025-12-03 18:20:33 -0600

    oauth protected resource document

--- backend/crates/server/src/auth_n/oidc/routes/discovery.rs
+++ backend/crates/server/src/auth_n/oidc/routes/discovery.rs
@@ -29,6 +29,129 @@ pub struct WellKnownDiscoveryDocument {
     pub subject_types_supported: Vec<String>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct OAuthProtectedResourceDocument {
+    /**
+     * REQUIRED.  The protected resource's resource identifier, as
+     * defined in Section 1.2.
+     */
+    resource: String,
+
+    /**
+     * OPTIONAL.  JSON array containing a list of OAuth authorization
+     * server issuer identifiers, as defined in [RFC8414], for
+     * authorization servers that can be used with this protected
+     * resource.  Protected resources MAY choose not to advertise some
+     * supported authorization servers even when this parameter is used.
+     * In some use cases, the set of authorization servers will not be
+     * enumerable, in which case this metadata parameter would not be
+     * used.
+     */
+    authorization_servers: Option<Vec<String>>,
+
+    /**
+     * OPTIONAL.  URL of the protected resource's JSON Web Key (JWK) Set
+     * [JWK] document.  This contains public keys belonging to the
+     * protected resource, such as signing key(s) that the resource
+     * server uses to sign resource responses.  This URL MUST use the
+     * https scheme.  When both signing and encryption keys are made
+     * available, a use (public key use) parameter value is REQUIRED for
+     * all keys in the referenced JWK Set to indicate each key's intended
+     * usage.
+     */
+    jwks_uri: Option<String>,
+
+    /**
+     * RECOMMENDED.  JSON array containing a list of scope values, as
+     * defined in OAuth 2.0 [RFC6749], that are used in authorization
+     * requests to request access to this protected resource.  Protected
+     * resources MAY choose not to advertise some scope values supported
+     * even when this parameter is used.
+     */
+    scopes_supported: Option<Vec<String>>,
+
+    /**
+     * OPTIONAL.  JSON array containing a list of the supported methods
+     * of sending an OAuth 2.0 bearer token [RFC6750] to the protected
+     * resource.  Defined values are ["header", "body", "query"],
+     * corresponding to Sections 2.1, 2.2, and 2.3 of [RFC6750].  The
+     * empty array [] can be used to indicate that no bearer methods are
+     * supported.  If this entry is omitted, no default bearer methods
+     * supported are implied, nor does its absence indicate that they are
+     * not supported.
+     */
+    bearer_methods_supported: Option<Vec<String>>,
+
+    /**
+     * OPTIONAL.  JSON array containing a list of the JWS [JWS] signing
+     * algorithms (alg values) [JWA] supported by the protected resource
+     * for signing resource responses, for instance, as described in
+     * [FAPI.MessageSigning].  No default algorithms are implied if this
+     * entry is omitted.  The value none MUST NOT be used.
+     */
+    resource_signing_alg_values_supported: Option<Vec<String>>,
+
+    /**
+     * Human-readable name of the protected resource intended for display
+     * to the end user.  It is RECOMMENDED that protected resource
+     * metadata include this field.  The value of this field MAY be
+     * internationalized, as described in Section 2.1.
+     */
+    resource_name: Option<String>,
+
+    /**
+     * OPTIONAL.  URL of a page containing human-readable information
+     * that developers might want or need to know when using the
+     * protected resource.  The value of this field MAY be
+     * internationalized, as described in Section 2.1.
+     */
+    resource_documentation: Option<String>,
+
+    /**
+     * OPTIONAL.  URL of a page containing human-readable information
+     * about the protected resource's requirements on how the client can
+     * use the data provided by the protected resource.  The value of
+     * this field MAY be internationalized, as described in Section 2.1.
+     */
+    resource_policy_uri: Option<String>,
+
+    /**
+     * OPTIONAL.  URL of a page containing human-readable information
+     * about the protected resource's terms of service.  The value of
+     * this field MAY be internationalized, as described in Section 2.1.
+     */
+    resource_tos_uri: Option<String>,
+
+    /**
+     * OPTIONAL.  Boolean value indicating protected resource support for
+     * mutual-TLS client certificate-bound access tokens [RFC8705].  If
+     * omitted, the default value is false.
+     */
+    tls_client_certificate_bound_access_tokens: Option<bool>,
+
+    /**
+     * OPTIONAL.  JSON array containing a list of the authorization
+     * details type values supported by the resource server when the
+     * authorization_details request parameter [RFC9396] is used.
+     */
+    authorization_details_types_supported: Option<Vec<String>>,
+
+    /**
+     * OPTIONAL.  JSON array containing a list of the JWS alg values
+     * (from the "JSON Web Signature and Encryption Algorithms" registry
+     * [IANA.JOSE]) supported by the resource server for validating
+     * Demonstrating Proof of Possession (DPoP) proof JWTs [RFC9449].
+     */
+    dpop_signing_alg_values_supported: Option<Vec<String>>,
+
+    /**
+     * OPTIONAL.  Boolean value specifying whether the protected resource
+     * always requires the use of DPoP-bound access tokens [RFC9449].  If
+     * omitted, the default value is false.
+     */
+    dpop_bound_access_tokens_required: Option<bool>,
+}
+
 fn construct_oidc_route(tenant: &TenantId, project: &ProjectId, path: &str) -> String {
     format!(
         "/w/{}/{}/api/v1/oidc{}",
@@ -36,6 +159,23 @@ fn construct_oidc_route(tenant: &TenantId, project: &ProjectId, path: &str) -> S
         project.as_ref(),
         path
     )
+}
+
+pub async fn oauth_protected_resource<
+    Repo: Repository + Send + Sync,
+    Search: SearchEngine + Send + Sync,
+    Terminology: FHIRTerminology + Send + Sync,
+>(
+    Cached(TenantIdentifier { tenant }): Cached<TenantIdentifier>,
+    Cached(ProjectIdentifier { project }): Cached<ProjectIdentifier>,
+    State(state): State<Arc<AppState<Repo, Search, Terminology>>>,
+) -> Result<Json<WellKnownDiscoveryDocument>, OIDCError> {
+    let api_url_string = state
+        .config
+        .get(crate::ServerEnvironmentVariables::APIURI)
+        .unwrap_or_default();
+
+    todo!();
 }
 
 pub async fn openid_configuration<
