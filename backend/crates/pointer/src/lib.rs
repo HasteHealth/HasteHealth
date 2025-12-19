65e1f370bbe9770578fc5886e6da19b9aac05f04
confused-moniker <ralph@iguhealth.app>
2025-12-19 12:36:34 -0600

    mod

--- backend/crates/pointer/src/lib.rs
+++ backend/crates/pointer/src/lib.rs
@@ -1,15 +1,25 @@
-struct Pointer<'a, T> {
-    pub value: Option<&'a T>,
-    pub path: &'a str,
+use haste_reflect::MetaValue;
+pub struct Pointer<'a> {
+    pub value: Option<&'a dyn MetaValue>,
+    pub path: String,
 }
 
-impl<'a, T> Pointer<'a, T> {
-    pub fn root(value: &'a T) -> Self {
+fn path_descend(path: &str, key: &str) -> String {
+    format!("{}/{}", path, key)
+}
+
+impl<'a> Pointer<'a> {
+    pub fn root(value: &'a dyn MetaValue) -> Self {
         Pointer {
             value: Some(value),
-            path: "/",
+            path: "/".to_string(),
         }
     }
 
-    pub fn descend(&self, key: &str) -> Self {}
+    pub fn descend(&self, key: &str) -> Pointer<'a> {
+        Self {
+            path: path_descend(self.path.as_str(), key),
+            value: self.value.and_then(|v| v.get_field(key)),
+        }
+    }
 }
