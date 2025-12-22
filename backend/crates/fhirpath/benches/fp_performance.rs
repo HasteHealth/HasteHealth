fa86322587c75697b081de91df4bb1450dc301c9
confused-moniker <ralph@iguhealth.app>
2025-12-22 16:53:23 -0600

    mod

--- backend/crates/fhirpath/benches/fp_performance.rs
+++ backend/crates/fhirpath/benches/fp_performance.rs
@@ -16,29 +16,26 @@ fn fp_performance_simple(c: &mut Criterion) {
         })]),
         ..Default::default()
     };
-    let engine = FPEngine::new();
+
     c.bench_function("fp_performance_simple", |b| {
-        b.iter(|| engine.evaluate("Patient.name.given", vec![&root]).unwrap())
+        b.iter(|| FPEngine::evaluate("Patient.name.given", vec![&root]).unwrap())
     });
 }
 
 fn parser_test_performance(c: &mut Criterion) {
-    let engine = FPEngine::new();
     c.bench_function("parser_test_performance", |b| {
-        b.iter(|| engine.evaluate("1 + 2 * (3 - 4) / 5", vec![]).unwrap())
+        b.iter(|| FPEngine::evaluate("1 + 2 * (3 - 4) / 5", vec![]).unwrap())
     });
 }
 
 fn parser_test_complex(c: &mut Criterion) {
-    let engine = FPEngine::new();
     c.bench_function("parser_test_complex",
     |b| b.iter(|| engine.evaluate("$this.field + %test._asdf.test(45, $this.field) * 64 * $this.where($this.field = '23'.length())", vec![]).unwrap()));
 }
 
 fn parser_test_simple(c: &mut Criterion) {
-    let engine = FPEngine::new();
     c.bench_function("parser_test_simple", |b| {
-        b.iter(|| engine.evaluate("$this.field", vec![]).unwrap())
+        b.iter(|| FPEngine::evaluate("$this.field", vec![]).unwrap())
     });
 }
 
