ddc0da632ab9615a121d4204baec09031b5f64c0
confused-moniker <ralph@iguhealth.app>
2025-12-23 19:17:18 -0600

    mod

--- backend/crates/access-control/src/engine/rule_engine/pip.rs
+++ backend/crates/access-control/src/engine/rule_engine/pip.rs
@@ -3,23 +3,20 @@
 use haste_fhir_client::FHIRClient;
 use haste_fhir_model::r4::generated::resources::AccessPolicyV2;
 use haste_fhir_operation_error::OperationOutcomeError;
+use haste_reflect::MetaValue;
 use std::sync::Arc;
 
 use crate::context::PolicyContext;
 
-#[allow(unused)]
-pub struct PIPResult {}
-
-#[allow(unused)]
 pub async fn pip<'a, CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
     _policy_context: Arc<PolicyContext<CTX, Client>>,
     _policy: &AccessPolicyV2,
     variable_id: &str,
-) -> Result<PIPResult, OperationOutcomeError> {
+) -> Result<Option<Box<dyn MetaValue>>, OperationOutcomeError> {
     match variable_id {
         "user" => None,
         _ => Some("test".to_string()),
     };
 
-    Ok(PIPResult {})
+    Ok(None)
 }
