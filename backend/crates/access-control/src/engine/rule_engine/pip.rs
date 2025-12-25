425c299c81714391ff764e79125270f12a3c35df
confused-moniker <ralph@iguhealth.app>
2025-12-25 13:51:38 -0600

    mod

--- backend/crates/access-control/src/engine/rule_engine/pip.rs
+++ backend/crates/access-control/src/engine/rule_engine/pip.rs
@@ -6,10 +6,11 @@ use haste_fhir_model::r4::generated::{
     terminology::AccessPolicyAttributeOperationTypes,
 };
 use haste_fhir_operation_error::OperationOutcomeError;
+use haste_pointer::Pointer;
 use haste_reflect::MetaValue;
 use std::sync::Arc;
 
-use crate::context::PolicyContext;
+use crate::{context::PolicyContext, engine::rule_engine::expression::evaluate_expression};
 
 fn find_attribute<'a>(
     access_policy: &'a AccessPolicyV2,
@@ -22,11 +23,22 @@ fn find_attribute<'a>(
     })
 }
 
-pub async fn pip<'a, CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
-    _policy_context: Arc<PolicyContext<CTX, Client>>,
-    access_policy: &AccessPolicyV2,
+pub async fn pip<
+    'a,
+    CTX: Sync + Send + 'static,
+    Client: FHIRClient<CTX, OperationOutcomeError> + 'static,
+>(
+    policy_context: Arc<PolicyContext<CTX, Client>>,
+    pointer: Pointer<AccessPolicyV2, AccessPolicyV2>,
     variable_id: &str,
 ) -> Result<Option<Box<dyn MetaValue>>, OperationOutcomeError> {
+    let access_policy = pointer.clone().value().ok_or_else(|| {
+        OperationOutcomeError::fatal(
+            haste_fhir_model::r4::generated::terminology::IssueType::Invalid(None),
+            "Pointer root does not contain an AccessPolicyV2 resource.".to_string(),
+        )
+    })?;
+
     let Some(attribute) = find_attribute(access_policy, variable_id) else {
         return Ok(None);
     };
@@ -43,7 +55,18 @@ pub async fn pip<'a, CTX, Client: FHIRClient<CTX, OperationOutcomeError>>(
 
     match attribute_operation.type_.as_ref() {
         AccessPolicyAttributeOperationTypes::Read(_) => {
-            println!("asdf");
+            let path_expression = attribute_operation.path.as_ref().ok_or_else(|| {
+                OperationOutcomeError::fatal(
+                    haste_fhir_model::r4::generated::terminology::IssueType::Invalid(None),
+                    format!(
+                        "Attribute operation path is not specified for attribute '{}'.",
+                        variable_id
+                    ),
+                )
+            })?;
+
+            let path = evaluate_expression(policy_context, pointer, &path_expression).await?;
+
             Ok(None)
         }
         AccessPolicyAttributeOperationTypes::SearchSystem(_) => {
