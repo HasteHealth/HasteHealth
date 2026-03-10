3e902826bf31e419cf803a1eb148d41f999c394f
confused-moniker <ralph@iguhealth.app>
2026-03-10 16:44:12 -0500

    mod

--- backend/crates/fhir-profiling/src/element.rs
+++ backend/crates/fhir-profiling/src/element.rs
@@ -5,14 +5,68 @@ use haste_fhir_model::r4::generated::{
 };
 use haste_fhir_operation_error::OperationOutcomeError;
 use haste_pointer::{Key, Path};
+use haste_reflect::MetaValue;
 
 use crate::FHIRProfileCTX;
+
+/**
+ * Check if the element is constrained to profiles type.
+ * @param element ElementDefinition to check
+ * @param type The type found on the element.
+ * @returns true|false as to whether the element is constrained to the type.
+ */
+fn validate_type_if_multiple_types_constrained<'a>(
+    ctx: FHIRProfileCTX<'a, impl CanonicalResolver>,
+    element: &ElementDefinition,
+    type_: &str,
+) -> bool {
+    let Some(types) = &element.type_ else {
+        return true;
+    };
+
+    if types
+        .iter()
+        .find(|t| t.code.value.as_ref().map(|s| s.as_str()) == Some(type_))
+        .is_some()
+    {
+        true
+    } else if type_ == "Element" {
+        false
+    } else {
+        false
+    }
+}
+
+fn _validate_cardinality(
+    value_cardinality: usize,
+    // Max could be '*' which is any number of elements.
+    element_cardinality: (usize, Option<&str>),
+) -> bool {
+    false
+}
+
+fn validate_cardinality<'a>(
+    ctx: FHIRProfileCTX<'a, impl CanonicalResolver>,
+    element: &ElementDefinition,
+    value: Option<&'a dyn MetaValue>,
+) -> Vec<OperationOutcomeIssue> {
+    match value {
+        Some(v) => {
+            v.flatten().len();
+        }
+        None => {}
+    };
+
+    vec![]
+}
 
 pub async fn validate_element<'a>(
     ctx: FHIRProfileCTX<'a, impl CanonicalResolver>,
     element_pointer: Path,
-    _value_pointer: Path,
+    value_pointer: Path,
 ) -> Result<Vec<OperationOutcomeIssue>, OperationOutcomeError> {
+    let value = value_pointer.get(ctx.root);
+
     let Some((elements_pointer, Key::Index(index))) = element_pointer.ascend() else {
         return Err(OperationOutcomeError::error(
             IssueType::Exception(None),
@@ -21,7 +75,7 @@ pub async fn validate_element<'a>(
     };
 
     let elements = elements_pointer
-        .get::<Vec<Box<ElementDefinition>>>(ctx.root)
+        .get_typed::<Vec<Box<ElementDefinition>>>(ctx.root)
         .ok_or_else(|| {
             OperationOutcomeError::error(
                 IssueType::Exception(None),
@@ -32,7 +86,7 @@ pub async fn validate_element<'a>(
     let _children = traversal::ele_index_to_child_indices(elements, index)
         .map_err(|error| OperationOutcomeError::error(IssueType::Exception(None), error))?;
 
-    let _element = element_pointer.get::<Box<ElementDefinition>>(ctx.root);
+    let _element = element_pointer.get_typed::<Box<ElementDefinition>>(ctx.root);
 
     Ok(vec![])
 }
