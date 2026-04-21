75b03771d6931ea09982814f342dbd4941efef43
confused-moniker <ralph@iguhealth.app>
2026-04-21 16:38:30 -0500

    comment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -126,6 +126,7 @@ fn main() -> Result<(), OperationOutcomeError> {
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
+        // 8MB stack size
         .thread_stack_size(1024 * 8000)
         .build()
         .unwrap()
