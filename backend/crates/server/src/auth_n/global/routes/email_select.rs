c292f6f2d862afae76ebf88e7de8a6d7ff7d806d
confused-moniker <ralph@iguhealth.app>
2026-04-25 12:50:02 -0500

    mod

--- backend/crates/server/src/auth_n/global/routes/email_select.rs
+++ backend/crates/server/src/auth_n/global/routes/email_select.rs
@@ -1,4 +1,6 @@
-use crate::{extract::path_tenant::TenantIdentifier, services::AppState};
+use crate::{
+    extract::path_tenant::TenantIdentifier, services::AppState, ui::components::page_html,
+};
 use axum::{
     Form,
     extract::{OriginalUri, State},
@@ -10,7 +12,7 @@ use haste_fhir_operation_error::OperationOutcomeError;
 use haste_fhir_search::SearchEngine;
 use haste_fhir_terminology::FHIRTerminology;
 use haste_repository::Repository;
-use maud::Markup;
+use maud::{Markup, html};
 use serde::Deserialize;
 use std::sync::Arc;
 use tower_sessions::Session;
@@ -29,7 +31,28 @@ pub async fn email_select_get<
     Cached(TenantIdentifier { tenant }): Cached<TenantIdentifier>,
     uri: OriginalUri,
 ) -> Result<Markup, OperationOutcomeError> {
-    todo!();
+    Ok(page_html(html! {
+        (banner("Enter your tenant identifier", None))
+        div class="w-full bg-white rounded-lg shadow md:mt-0 xl:p-0 w-md sm:max-w-md text-slate-700" {
+            div class="p-6 space-y-4 md:space-y-6 sm:p-8" {
+                form class="space-y-2" action=(action_url) method="POST" {
+                    div class="grid grid-cols-4 gap-1" {
+                        div class="col-span-4" {
+                            label for="tenant" class="block text-sm font-medium text-slate-600" { "Tenant" }
+                            input type="tenant" id="tenant" class="bg-gray-50 border border-gray-300 text-slate-900 sm:text-sm rounded-lg focus:ring-orange-600 focus:border-orange-600 block w-full p-2.5 " placeholder="Tenant id" required name="tenant" value="" {}
+                        }
+                    }
+
+                    div class="space-y-4" {
+                        button type="submit" class="w-full text-white bg-orange-500 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center " { "Continue" }
+                        div class="flex items-center justify-start" {
+                            a href=(signup_url) class="text-sm font-medium text-orange-600 hover:underline " { "Sign up" }
+                        }
+                    }
+                }
+            }
+        }
+    }).into_response())
 }
 
 #[derive(Deserialize)]
