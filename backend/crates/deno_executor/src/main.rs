04e68546b710a95246b80d80359d8abaf632f115
confused-moniker <ralph@iguhealth.app>
2026-04-28 20:49:57 -0500

    alt

--- backend/crates/deno_executor/src/main.rs
+++ backend/crates/deno_executor/src/main.rs
@@ -1,9 +1,17 @@
+use deno_core::error::ModuleLoaderError;
+use deno_core::{
+    ModuleLoadOptions, ModuleLoadReferrer, ModuleLoader, ModuleSource, ModuleType, resolve_import,
+};
 // main.rs
 use deno_core::{error::AnyError, extension};
 use std::rc::Rc;
 
-use deno_core::PollEventLoopOptions;
+use deno_ast::{MediaType, ModuleSpecifier};
+use deno_ast::{ParseParams, SourceMapOption};
+use deno_core::ModuleLoadResponse;
+use deno_core::ModuleSourceCode;
 use deno_core::op2;
+use deno_error::JsErrorBox;
 
 #[op2]
 #[string]
@@ -19,6 +27,95 @@ async fn op_write_file(#[string] path: String, #[string] contents: String) -> Op
     Some("".to_string())
 }
 
+struct TsModuleLoader;
+impl ModuleLoader for TsModuleLoader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        _kind: deno_core::ResolutionKind,
+    ) -> Result<deno_core::ModuleSpecifier, ModuleLoaderError> {
+        resolve_import(specifier, referrer).map_err(JsErrorBox::from_err)
+    }
+
+    fn load(
+        &self,
+        module_specifier: &ModuleSpecifier,
+        _maybe_referrer: Option<&ModuleLoadReferrer>,
+        _options: ModuleLoadOptions,
+    ) -> ModuleLoadResponse {
+        fn load(module_specifier: &ModuleSpecifier) -> Result<ModuleSource, ModuleLoaderError> {
+            let path = module_specifier
+                .to_file_path()
+                .map_err(|_| JsErrorBox::generic("Only file:// URLs are supported."))?;
+
+            let media_type = MediaType::from_path(&path);
+            let (module_type, should_transpile) = match MediaType::from_path(&path) {
+                MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
+                    (ModuleType::JavaScript, false)
+                }
+                MediaType::Jsx => (ModuleType::JavaScript, true),
+                MediaType::TypeScript
+                | MediaType::Mts
+                | MediaType::Cts
+                | MediaType::Dts
+                | MediaType::Dmts
+                | MediaType::Dcts
+                | MediaType::Tsx => (ModuleType::JavaScript, true),
+                MediaType::Json => (ModuleType::Json, false),
+                _ => {
+                    return Err(JsErrorBox::generic(format!(
+                        "Unknown extension {:?}",
+                        path.extension()
+                    )));
+                }
+            };
+
+            let code = std::fs::read_to_string(&path).map_err(JsErrorBox::from_err)?;
+            let code = if should_transpile {
+                let parsed = deno_ast::parse_module(ParseParams {
+                    specifier: module_specifier.clone(),
+                    text: code.into(),
+                    media_type,
+                    capture_tokens: false,
+                    scope_analysis: false,
+                    maybe_syntax: None,
+                })
+                .map_err(JsErrorBox::from_err)?;
+                let res = parsed
+                    .transpile(
+                        &deno_ast::TranspileOptions {
+                            imports_not_used_as_values: deno_ast::ImportsNotUsedAsValues::Remove,
+                            decorators: deno_ast::DecoratorsTranspileOption::Ecma,
+                            ..Default::default()
+                        },
+                        &deno_ast::TranspileModuleOptions { module_kind: None },
+                        &deno_ast::EmitOptions {
+                            source_map: SourceMapOption::Separate,
+                            inline_sources: true,
+                            ..Default::default()
+                        },
+                    )
+                    .map_err(JsErrorBox::from_err)?;
+                let res = res.into_source();
+                // let source_map = res.source_map.unwrap().into_bytes();
+
+                res.text
+            } else {
+                code
+            };
+            Ok(ModuleSource::new(
+                module_type,
+                ModuleSourceCode::String(code.into()),
+                module_specifier,
+                None,
+            ))
+        }
+
+        ModuleLoadResponse::Sync(load(module_specifier))
+    }
+}
+
 async fn run_js(file_path: &str) -> Result<(), AnyError> {
     let main_module = deno_core::resolve_path(file_path, &std::env::current_dir()?)?;
     extension!(
@@ -32,7 +129,7 @@ async fn run_js(file_path: &str) -> Result<(), AnyError> {
     );
 
     let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
-        module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+        module_loader: Some(Rc::new(TsModuleLoader)),
         extensions: vec![runjs::init()],
         ..Default::default()
     });
@@ -52,7 +149,7 @@ fn main() {
         .enable_all()
         .build()
         .unwrap();
-    if let Err(error) = runtime.block_on(run_js("./example.js")) {
+    if let Err(error) = runtime.block_on(run_js("./example.ts")) {
         eprintln!("error: {}", error);
     }
 }
