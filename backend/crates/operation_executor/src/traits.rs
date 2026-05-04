434068e6fb7541a73d69904c023519270889ccf2
confused-moniker <ralph@iguhealth.app>
2026-05-04 17:24:24 -0500

    mod

--- backend/crates/operation_executor/src/traits.rs
+++ backend/crates/operation_executor/src/traits.rs
@@ -1,13 +1,13 @@
-use haste_fhir_client::{
-    FHIRClient,
-    request::{InvocationRequest, InvokeResponse},
-};
+use haste_fhir_client::{FHIRClient, request::InvocationRequest};
 use haste_fhir_model::r4::generated::resources::{OperationDefinition, Parameters};
 use haste_fhir_operation_error::OperationOutcomeError;
 use std::sync::Arc;
 
 pub trait OperationExecutor {
-    fn execute_operation<CTX, Client: FHIRClient<CTX, OperationOutcomeError> + 'static>(
+    fn execute_operation<
+        CTX: Clone + Send + 'static,
+        Client: FHIRClient<CTX, OperationOutcomeError> + 'static,
+    >(
         &self,
         context: CTX,
         client: Arc<Client>,
