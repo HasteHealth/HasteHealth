c4f2d69ae0af2c19cb2dcb2195daedfbeb649b9f
confused-moniker <ralph@iguhealth.app>
2026-06-03 21:53:02 -0500

    mod

--- backend/crates/hl7v2/src/serialize.rs
+++ backend/crates/hl7v2/src/serialize.rs
@@ -2,6 +2,55 @@ use crate::parser::ParsedHL7V2Header;
 
 impl From<ParsedHL7V2Header> for String {
     fn from(value: ParsedHL7V2Header) -> Self {
-        todo!()
+        let hl7v2_message = value.0;
+        let field_seperator = hl7v2_message
+            .field_separator
+            .value
+            .unwrap_or('|'.to_string());
+        let mut result = [
+            "MSH",
+            &field_seperator,
+            &hl7v2_message
+                .encodingCharacters
+                .value
+                .as_ref()
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+            hl7v2_message
+                .sendingApplication
+                .as_ref()
+                .and_then(|s| s.value.as_ref())
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+            hl7v2_message
+                .sendingFacility
+                .as_ref()
+                .and_then(|s| s.value.as_ref())
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+            hl7v2_message
+                .receivingApplication
+                .as_ref()
+                .and_then(|s| s.value.as_ref())
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+            hl7v2_message
+                .receivingFacility
+                .as_ref()
+                .and_then(|s| s.value.as_ref())
+                .map(|s| s.as_str())
+                .unwrap_or(""),
+        ]
+        .join(&field_seperator);
+
+        if let Some(timestamp) = value.timestamp.value {
+            result.push_str(&timestamp);
+        }
+
+        if let Some(security) = value.security.value {
+            result.push_str(&security);
+        }
+
+        result
     }
 }
