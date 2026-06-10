84e16a34d50e770f71d3bf60cbc4751b2db14c2b
confused-moniker <ralph@iguhealth.app>
2026-06-10 18:03:36 -0500

    more conversions

--- backend/crates/fhir-converter/src/jinja_extensions/conversions/hl7v2.rs
+++ backend/crates/fhir-converter/src/jinja_extensions/conversions/hl7v2.rs
@@ -2,9 +2,10 @@ use std::sync::{Arc, LazyLock};
 
 use haste_fhir_model::r4::generated::resources::{
     HL7V2, HL7V2Segments, HL7V2SegmentsFields, HL7V2SegmentsFieldsValue,
+    HL7V2SegmentsFieldsValueValue,
 };
 use haste_hl7v2::serialize::{
-    EncodingInformation, SerializeMessage, segment_field_repititon_to_string,
+    EncodingInformation, SerializeMessage, component_to_string, segment_field_repititon_to_string,
     segment_field_to_string, segment_to_string,
 };
 use minijinja::{
@@ -119,8 +120,22 @@ impl<'a> Object for JHL7V2SegmentsFields<'a> {
     }
 
     fn get_value(self: &Arc<Self>, key: &Value) -> Option<Value> {
-        let _ = key;
-        None
+        let index = key.as_usize()?;
+
+        // if value is present pass key down chain otherwise treat as repetition which requires an indice.
+        if let Some(field_value) = self.0.value.as_ref() {
+            Arc::new(JHL7V2SegmentsFieldsValue(field_value)).get_value(key)
+        } else {
+            self.0.repetitions.as_ref()?.get(index).map(|field| {
+                Value::from_dyn_object(unsafe {
+                    let field_value = std::mem::transmute::<
+                        &HL7V2SegmentsFieldsValue,
+                        &'static HL7V2SegmentsFieldsValue,
+                    >(field);
+                    Arc::new(JHL7V2SegmentsFieldsValue(field_value))
+                })
+            })
+        }
     }
 
     fn get_value_by_str(self: &Arc<Self>, key: &str) -> Option<Value> {
@@ -143,8 +158,30 @@ impl<'a> Object for JHL7V2SegmentsFieldsValue<'a> {
     }
 
     fn get_value(self: &Arc<Self>, key: &Value) -> Option<Value> {
-        let _ = key;
-        None
+        let index = key.as_usize()?;
+
+        // Edge case if single value and index is 0, return the value instead of the repetition
+        if index == 0
+            && let Some(component) = self.0.value.as_ref()
+        {
+            Some(Value::from_dyn_object(unsafe {
+                let components = std::mem::transmute::<
+                    &HL7V2SegmentsFieldsValueValue,
+                    &'static HL7V2SegmentsFieldsValueValue,
+                >(component);
+                Arc::new(JHL7V2SegmentsFieldComponent(components))
+            }))
+        } else {
+            self.0.components.as_ref()?.get(index).map(|component| {
+                Value::from_dyn_object(unsafe {
+                    let components = std::mem::transmute::<
+                        &HL7V2SegmentsFieldsValueValue,
+                        &'static HL7V2SegmentsFieldsValueValue,
+                    >(component);
+                    Arc::new(JHL7V2SegmentsFieldComponent(components))
+                })
+            })
+        }
     }
 
     fn get_value_by_str(self: &Arc<Self>, key: &str) -> Option<Value> {
@@ -159,6 +196,59 @@ impl<'a> Object for JHL7V2SegmentsFieldsValue<'a> {
             f,
             "{}",
             segment_field_repititon_to_string(&*DEFAULT_ENCODING, self.0)
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct JHL7V2SegmentsFieldComponent<'a>(&'a HL7V2SegmentsFieldsValueValue);
+impl<'a> Object for JHL7V2SegmentsFieldComponent<'a> {
+    fn repr(self: &Arc<Self>) -> ObjectRepr {
+        ObjectRepr::Plain
+    }
+    fn get_value(self: &Arc<Self>, key: &Value) -> Option<Value> {
+        let index = key.as_usize()?;
+        if index == 0
+            && let Some(value) = self.0.value.as_ref()
+        {
+            Some(Value::from_safe_string(
+                value
+                    .value
+                    .as_ref()
+                    .map(|s| s.as_str())
+                    .unwrap_or("")
+                    .to_string(),
+            ))
+        } else {
+            self.0
+                .subcomponents
+                .as_ref()?
+                .get(index)
+                .map(|subcomponent| {
+                    Value::from_safe_string(
+                        subcomponent
+                            .value
+                            .as_ref()
+                            .map(|s| s.as_str())
+                            .unwrap_or("")
+                            .to_string(),
+                    )
+                })
+        }
+    }
+
+    fn get_value_by_str(self: &Arc<Self>, key: &str) -> Option<Value> {
+        self.get_value(&Value::from(key))
+    }
+
+    fn render(self: &Arc<Self>, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        Self: Sized + 'static,
+    {
+        write!(
+            f,
+            "{}",
+            component_to_string(&*DEFAULT_ENCODING, self.0).unwrap_or("".to_string())
         )
     }
 }
--- backend/crates/fhir-converter/src/lib.rs
+++ backend/crates/fhir-converter/src/lib.rs
@@ -20,7 +20,7 @@ pub fn fhir_converter2() {
 
     env.add_template(
         "hello",
-        "{{ 'Na '|repeat(3) | hl7v2_segments }} {{ name }} {{ hl7v2.MSH[0].4 }}!",
+        "{{ 'Na '|repeat(3) | hl7v2_segments }} {{ name }} {{ hl7v2.PID[0][4][0] }}!",
     )
     .unwrap();
     let tmpl = env.get_template("hello").unwrap();
