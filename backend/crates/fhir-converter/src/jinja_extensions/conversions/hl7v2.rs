43606891338acf92242724fd2b9dac474de0aabe
confused-moniker <ralph@iguhealth.app>
2026-06-10 17:26:10 -0500

    mod

--- backend/crates/fhir-converter/src/jinja_extensions/conversions/hl7v2.rs
+++ backend/crates/fhir-converter/src/jinja_extensions/conversions/hl7v2.rs
@@ -1,7 +1,12 @@
-use std::sync::Arc;
+use std::sync::{Arc, LazyLock};
 
-use haste_fhir_model::r4::generated::resources::{HL7V2, HL7V2Segments};
-use haste_hl7v2::serialize::SerializeMessage;
+use haste_fhir_model::r4::generated::resources::{
+    HL7V2, HL7V2Segments, HL7V2SegmentsFields, HL7V2SegmentsFieldsValue,
+};
+use haste_hl7v2::serialize::{
+    EncodingInformation, SerializeMessage, segment_field_repititon_to_string,
+    segment_field_to_string, segment_to_string,
+};
 use minijinja::{
     Value,
     value::{Enumerator, Object, ObjectRepr},
@@ -34,7 +39,7 @@ impl Object for JHL7V2 {
                 return None;
             }
 
-            return Some(Value::from(vec![
+            return Some(Value::from(
                 found_segments
                     .into_iter()
                     .map(|segment| {
@@ -47,7 +52,7 @@ impl Object for JHL7V2 {
                         })
                     })
                     .collect::<Vec<_>>(),
-            ]));
+            ));
         }
 
         None
@@ -66,9 +71,73 @@ impl Object for JHL7V2 {
     }
 }
 
+static DEFAULT_ENCODING: LazyLock<EncodingInformation> = LazyLock::new(|| EncodingInformation {
+    field_separator: "|".to_string(),
+    component_separator: "^".to_string(),
+    repetition_separator: "~".to_string(),
+    escape_character: "\\".to_string(),
+    subcomponent_separator: "&".to_string(),
+});
+
 #[derive(Debug)]
 pub struct JHL7V2Segment<'a>(&'a HL7V2Segments);
 impl<'a> Object for JHL7V2Segment<'a> {
+    fn repr(self: &Arc<Self>) -> ObjectRepr {
+        ObjectRepr::Map
+    }
+
+    fn get_value(self: &Arc<Self>, key: &Value) -> Option<Value> {
+        if let Some(index) = key.as_usize() {
+            self.0.fields.as_ref()?.get(index).map(|field| {
+                Value::from_dyn_object(unsafe {
+                    let field = std::mem::transmute::<
+                        &HL7V2SegmentsFields,
+                        &'static HL7V2SegmentsFields,
+                    >(field);
+
+                    Arc::new(JHL7V2SegmentsFields(field))
+                })
+            })
+        } else {
+            None
+        }
+    }
+
+    fn render(self: &Arc<Self>, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        Self: Sized + 'static,
+    {
+        write!(f, "{}", segment_to_string(&*DEFAULT_ENCODING, self.0))
+    }
+}
+
+#[derive(Debug)]
+pub struct JHL7V2SegmentsFields<'a>(&'a HL7V2SegmentsFields);
+impl<'a> Object for JHL7V2SegmentsFields<'a> {
+    fn repr(self: &Arc<Self>) -> ObjectRepr {
+        ObjectRepr::Map
+    }
+
+    fn get_value(self: &Arc<Self>, key: &Value) -> Option<Value> {
+        let _ = key;
+        None
+    }
+
+    fn get_value_by_str(self: &Arc<Self>, key: &str) -> Option<Value> {
+        self.get_value(&Value::from(key))
+    }
+
+    fn render(self: &Arc<Self>, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    where
+        Self: Sized + 'static,
+    {
+        write!(f, "{}", segment_field_to_string(&*DEFAULT_ENCODING, self.0))
+    }
+}
+
+#[derive(Debug)]
+pub struct JHL7V2SegmentsFieldsValue<'a>(&'a HL7V2SegmentsFieldsValue);
+impl<'a> Object for JHL7V2SegmentsFieldsValue<'a> {
     fn repr(self: &Arc<Self>) -> ObjectRepr {
         ObjectRepr::Map
     }
@@ -88,8 +157,8 @@ impl<'a> Object for JHL7V2Segment<'a> {
     {
         write!(
             f,
-            "HL7V2Segment(id: {})",
-            self.0.id.value.as_ref().map(|s| s.as_str()).unwrap_or("")
+            "{}",
+            segment_field_repititon_to_string(&*DEFAULT_ENCODING, self.0)
         )
     }
 }
