222de56b412eed268f72652e5a6715ad944c7696
confused-moniker <ralph@iguhealth.app>
2026-06-24 16:15:46 -0500

    mod

---
+++ frontend/packages/admin-app/src/views/Analytics/ViewDefinitionEditor.tsx
@@ -0,0 +1,20 @@
+import { ViewDefinitionSqlRunner } from "@haste-health/components";
+import { getClient } from "../../db/client";
+import { useAtomValue } from "jotai";
+import { R4 } from "@haste-health/fhir-types/versions";
+
+export default function ViewDefinitionEditor() {
+  const client = useAtomValue(getClient);
+  return (
+    <ViewDefinitionSqlRunner
+      client={client}
+      defaultPageSize={10}
+      fhirVersion={R4}
+      resources={[
+        {
+          resourceType: "Patient",
+        },
+      ]}
+    />
+  );
+}
