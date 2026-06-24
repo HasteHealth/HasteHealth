e9a8e0274e87e5a5385cd6f278c85a1f4ffe3e33
confused-moniker <ralph@iguhealth.app>
2026-06-24 16:24:38 -0500

    mod

--- frontend/packages/admin-app/src/views/Analytics/ViewDefinitionEditor.tsx
+++ frontend/packages/admin-app/src/views/Analytics/ViewDefinitionEditor.tsx
@@ -2,6 +2,7 @@ import { ViewDefinitionSqlRunner } from "@haste-health/components";
 import { getClient } from "../../db/client";
 import { useAtomValue } from "jotai";
 import { R4 } from "@haste-health/fhir-types/versions";
+import { date } from "@haste-health/fhir-types/r4/types";
 
 export default function ViewDefinitionEditor() {
   const client = useAtomValue(getClient);
@@ -13,7 +14,8 @@ export default function ViewDefinitionEditor() {
       resources={[
         {
           resourceType: "Patient",
-          name: [{ given: ["John"], family: "Doe" }],
+          name: [{ given: ["John", "Bob"], family: "Doe" }],
+          birthDate: "1990-01-01" as date,
         },
       ]}
     />
