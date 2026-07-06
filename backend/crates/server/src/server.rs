faa08ae3454110ce0c64a2810b2735d5706ec204
confused-moniker <ralph@iguhealth.app>
2026-07-06 12:42:09 -0500

    mod

--- backend/crates/server/src/server.rs
+++ backend/crates/server/src/server.rs
@@ -198,16 +198,15 @@ pub async fn server(
                 )),
         );
 
-    let project_router = Router::new()
-        .merge(protected_resources_router)
-        .nest(
-            "/oidc",
-            auth_n::oidc::routes::create_router(shared_state.clone()),
-        )
-        .nest(
-            "/mfa",
-            auth_n::mfa::routes::create_router(shared_state.clone()),
-        );
+    let project_router = Router::new().merge(protected_resources_router).nest(
+        "/oidc",
+        auth_n::oidc::routes::create_router(shared_state.clone()),
+    );
+
+    // .nest(
+    //     "/mfa",
+    //     auth_n::mfa::routes::create_router(shared_state.clone()),
+    // );
 
     let tenant_router = Router::new()
         .nest("/auth", auth_n::tenant::routes::create_router())
