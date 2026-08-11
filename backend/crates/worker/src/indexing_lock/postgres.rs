d9d059f2786301ade70579acca4d89fcb6675d31
Luni-4 <luni-4@hotmail.it>
2026-08-11 21:11:44 +0200

    Resolve clippy lints for worker crate

--- backend/crates/worker/src/indexing_lock/postgres.rs
+++ backend/crates/worker/src/indexing_lock/postgres.rs
@@ -41,7 +41,7 @@ impl IndexLockProvider<TenantId, TenantLockIndex> for PGConnection {
                 );
 
                 let mut separated = query_builder.separated(", ");
-                for tenant_id in tenants.iter() {
+                for tenant_id in &tenants {
                     separated.push_bind(tenant_id.as_ref());
                 }
 
@@ -56,7 +56,7 @@ impl IndexLockProvider<TenantId, TenantLockIndex> for PGConnection {
 
                 Ok(res)
             }
-            _ => Err(TenantLockIndexError::InvalidConnection.into()),
+            PGConnection::Pool(..) => Err(TenantLockIndexError::InvalidConnection.into()),
         }
     }
 
@@ -84,7 +84,7 @@ impl IndexLockProvider<TenantId, TenantLockIndex> for PGConnection {
 
                 Ok(())
             }
-            _ => Err(TenantLockIndexError::InvalidConnection.into()),
+            PGConnection::Pool(..) => Err(TenantLockIndexError::InvalidConnection.into()),
         }
     }
 }
