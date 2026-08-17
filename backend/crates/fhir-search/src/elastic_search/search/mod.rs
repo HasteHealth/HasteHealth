e10f8aa3608f053ebc8ab73f235cecdedc80d7c2
confused-moniker <ralph@haste.health>
2026-08-17 15:29:08 -0500

    mod

--- backend/crates/fhir-search/src/elastic_search/search/mod.rs
+++ backend/crates/fhir-search/src/elastic_search/search/mod.rs
@@ -388,11 +388,8 @@ async fn handle_result_parameter<ParameterResolver: SearchParameterResolve>(
             )
             .await?;
         }
-        "_elements" => {
-            // _elements is handled in middleware, not in the ES query.
-        }
-        "_summary" => {
-            // _summary is handled in middleware, not in the ES query.
+        "_summary" | "_elements" => {
+            // _elements and _summary are handled in middleware, not in the ES query.
         }
         _ => {
             return Err(QueryBuildError::UnsupportedParameter(result_param.name.clone()).into());
