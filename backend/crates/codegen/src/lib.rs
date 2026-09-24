3b26c0b514cd3e31d91eedb0fa558b7190e80b0f
Luni-4 <luni-4@hotmail.it>
2026-09-24 02:57:53 +0200

    Resolve `clippy` lints for `fhir-generated-ops` (#932)
    * Change codegen
    
    * Resolve clippy lints for fhir-generated-ops
    
    * Remove wrong FHIRString usage

--- backend/crates/codegen/src/lib.rs
+++ backend/crates/codegen/src/lib.rs
@@ -1,6 +1,7 @@
-pub mod search_param_cardinality;
-pub mod traversal;
+pub(crate) mod documentation;
 
+pub mod search_param_cardinality;
 pub mod testscript_gen;
+pub mod traversal;
 pub mod type_gen;
 pub mod utilities;
